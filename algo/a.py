6e18e235352e7e4fa2930ad4f57459f9801f949e
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-20 16:35:00 -0400

    'done'

--- algo/a.py
+++ algo/a.py
@@ -1,6 +1,14 @@
 
 
-
+ar = [2,3,4,4]
+val=0
+for i in range(len(ar)):
+    powss = 0
+    temp = 4
+    for j in range(len(ar)-i): 
+        powss = temp * temp
+    val = val + ar[i] * powss
+print(val)
 
 
 # -- end code --
