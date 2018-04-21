1f40c2e8526568d899740eaa3e617d773ad08a26
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-21 14:38:00 -0400

    dyanmic

--- algo/c.py
+++ algo/c.py
@@ -1,7 +1,16 @@
 import numpy as np
 
+# coin row dynamic 
+coin = [5,1,2,10,6,2]
+maxval = [0] *  len(coin)
+maxval[1] = coin[0]
 
-
-
+for i in range(2, len(coin)):
+    addcoin = coin[i] + maxval[i-2]
+    if addcoin > maxval[i-1]: 
+        maxval[i] = addcoin
+    else:
+        maxval[i] = maxval[i-1]
+print(maxval)
 
 # -- end code --
