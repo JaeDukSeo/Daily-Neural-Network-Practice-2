084b351a38b3a6754fc3aa399d050d7208b779cb
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-21 05:52:53 -0400

    'added'

--- algo/a.py
+++ algo/a.py
@@ -1,5 +1,5 @@
 
-
+import math
 ar = [2,3,4,4]
 val=0
 for i in range(len(ar)):
@@ -9,6 +9,21 @@ for i in range(len(ar)):
         powss = temp * temp
     val = val + ar[i] * powss
 print(val)
+
+
+def binar(n):
+    count = 1
+    while n > 1: 
+        count = count + 1
+        n = math.floor(n/2)
+    return count
+
+print(binar(4))
+print(binar(2))
+print(binar(3))
+print(binar(4))
+print(binar(5))
+
 
 
 # -- end code --
