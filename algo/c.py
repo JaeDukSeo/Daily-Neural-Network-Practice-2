5bcccc19cd45a1875d5f1fb2774b0c5535deb672
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-22 05:53:14 -0400

    added zeros

--- algo/c.py
+++ algo/c.py
@@ -13,4 +13,17 @@ for i in range(2, len(coin)):
         maxval[i] = maxval[i-1]
 print(maxval)
 
+# dynamic coin robot
+matrix  = np.array([
+    [0,0,0,0,1,0],
+    [0,1,0,1,0,0],
+    [0,0,0,1,0,1],
+    [0,0,1,0,0,1],
+    [1,0,0,0,1,0],
+])
+
+p = np.zeros_like(matrix)
+
+
+
 # -- end code --
