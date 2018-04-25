1cda09c3b3485fcfcdba4e7ad561b9c2000d1085
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-25 17:33:02 -0400

    omre

--- NeuralNetwork/WideResNet/a.py
+++ NeuralNetwork/WideResNet/a.py
@@ -1,12 +1,10 @@
 import tensorflow as tf
 import numpy as np
-import sys, os
-import cv2
+import sys, os,cv2
 from sklearn.utils import shuffle
 from scipy.misc import imread
 from scipy.misc import imresize
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from sklearn.preprocessing import OneHotEncoder
 from tensorflow.examples.tutorials.mnist import input_data
 from skimage.transform import resize
@@ -16,6 +14,54 @@ def d_tf_relu(x): return tf.cast(tf.greater(x,0),tf.float32)
 def tf_elu(x): return tf.nn.elu(x)
 def d_tf_elu(x): return tf.cast(tf.greater(x,0),tf.float32) + tf_elu(tf.cast(tf.less_equal(x,0),tf.float32)*x)
 
+
+# data
+data_location = "../../Dataset/Semanticdataset100/image/"
+train_data = []  # create an empty list
+for dirName, subdirList, fileList in sorted(os.walk(data_location)):
+    for filename in fileList:
+        if ".jpg" in filename.lower():
+            train_data.append(os.path.join(dirName,filename))
+
+data_location = "../../Dataset/Semanticdataset100/ground-truth/"
+train_data_gt = []  # create an empty list
+for dirName, subdirList, fileList in sorted(os.walk(data_location)):
+    for filename in fileList:
+        if ".png" in filename.lower() :
+            train_data_gt.append(os.path.join(dirName,filename))
+
+train_images = np.zeros(shape=(850,256,256,3))
+train_labels = np.zeros(shape=(850,256,256,1))
+
+for file_index in range(len(train_data)):
+    train_images[file_index,:,:]   = imresize(imread(train_data[file_index]),(256,256))
+    train_labels[file_index,:,:]   = np.expand_dims(imresize(imread(train_data_gt[file_index],mode='F',flatten=True),(256,256)),axis=2)
+
+train_images[:,:,:,0]  = (train_images[:,:,:,0] - train_images[:,:,:,0].min(axis=0)) / (train_images[:,:,:,0].max(axis=0) - train_images[:,:,:,0].min(axis=0))
+train_images[:,:,:,1]  = (train_images[:,:,:,1] - train_images[:,:,:,1].min(axis=0)) / (train_images[:,:,:,1].max(axis=0) - train_images[:,:,:,1].min(axis=0))
+train_images[:,:,:,2]  = (train_images[:,:,:,2] - train_images[:,:,:,2].min(axis=0)) / (train_images[:,:,:,2].max(axis=0) - train_images[:,:,:,2].min(axis=0))
+train_labels = (train_labels - train_labels.min(axis=0)) / (train_labels.max(axis=0) - train_labels.min(axis=0))
+
+for x in range(10):
+    plt.imshow(train_images[x,:,:,:])
+    plt.show()
+
+
+sys.exit()
+
+# class
+
+# hyper
+
+# graph
+# x = tf.placeholder()
+# y = tf.placeholder()
+
+
+
+# session
+with tf.Session() as sess: 
+    sess.run(tf.global_variables_initializer())
 
 
 
