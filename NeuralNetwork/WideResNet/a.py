ad2c1dd853a16d518779c1826da32b20a194b5ad
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-26 15:02:37 -0400

    Added

--- NeuralNetwork/WideResNet/a.py
+++ NeuralNetwork/WideResNet/a.py
@@ -23,7 +23,7 @@ def unpickle(file):
 
 # data
 mnist = input_data.read_data_sets('../../Dataset/MNIST/', one_hot=True)
-x_data, train_label, y_data, testing_label = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+x_data, train_label, y_data, test_label = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
 x_data = x_data.reshape(-1, 28, 28, 1)  # 28x28x1 input img
 y_data = y_data.reshape(-1, 28, 28, 1)  # 28x28x1 input img
 
@@ -46,10 +46,12 @@ class cnn():
 
     def feedforward(self,input,resadd=True):
         self.input  = input
+
         self.layer1  = tf.nn.conv2d(input,self.w1,strides=[1,1,1,1],padding='SAME')
         self.layerA1  = tf_relu(self.layer1) 
         self.layer2  = tf.nn.conv2d(self.layerA1,self.w2,strides=[1,1,1,1],padding='SAME')
         self.layerA2  = tf_relu(self.layer2) 
+
         if resadd: return self.layerA2 + input
         return self.layerA2 
 
@@ -84,59 +86,91 @@ class cnn():
 
         return grad_pass,grad_update    
 
+
+
+
 # hyper
 num_epoch = 100
-batch_size = 100
-print_size = 2
-learning_rate = 0.001
-beta1,beta2,adame = 0.9,0.999, 1e-8
+batch_size = 50
+print_size = 1
+learning_rate = 0.00001
+beta1,beta2,adame = 0.9,0.999,1e-8
+
+
+
+
+
+
 
 # class
-l1 = cnn(3,1,32)
+l1 = cnn(3,1,2)
 
-l2_1 = cnn(3,32,32)
-l2_2 = cnn(3,32,64)
+l2_1 = cnn(3,2,2)
+l2_2 = cnn(3,2,2)
+l2_3 = cnn(3,2,2)
+l2_4 = cnn(3,2,4)
 
-l3_1 = cnn(3,64,64)
-l3_2 = cnn(3,64,128)
+l3_1 = cnn(3,4,4)
+l3_2 = cnn(3,4,4)
+l3_3 = cnn(3,4,4)
+l3_4 = cnn(3,4,8)
 
-l4_1 = cnn(3,128,128)
-l4_2 = cnn(3,128,128)
-l4_3 = cnn(3,128,128)
-l4_4 = cnn(3,128,10)
+l4_1 = cnn(3,8,8)
+l4_2 = cnn(3,8,8)
+l4_3 = cnn(3,8,8)
+l4_4 = cnn(3,8,10)
+
+
+
+
+
+
+
 
 # graph
 x = tf.placeholder(shape=[None,32,32,1],dtype=tf.float32)
 y = tf.placeholder(shape=[None,10],dtype=tf.float32)
 
-layer1Input = tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
-layer1 = l1.feedforward(layer1Input,resadd=False)
+layer1 = l1.feedforward(x,resadd=False)
 
 layer2Input = tf.nn.max_pool(layer1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
 layer2_1 = l2_1.feedforward(layer2Input)
-layer2_2 = l2_2.feedforward(layer2_1,resadd=False)
+layer2_2 = l2_2.feedforward(layer2_1)
+layer2_3 = l2_3.feedforward(layer2_2)
+layer2_4 = l2_4.feedforward(layer2_3,resadd=False)
 
-layer3Input = tf.nn.max_pool(layer2_2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
+layer3Input = tf.nn.avg_pool(layer2_4,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
 layer3_1 = l3_1.feedforward(layer3Input)
-layer3_2 = l3_2.feedforward(layer3_1,resadd=False)
+layer3_2 = l3_2.feedforward(layer3_1)
+layer3Input2 = tf.nn.max_pool(layer3_2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
+layer3_3 = l3_3.feedforward(layer3Input2)
+layer3_4 = l3_4.feedforward(layer3_3,resadd=False)
 
-layer4Input = tf.nn.max_pool(layer3_2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
+layer4Input = tf.nn.avg_pool(layer3_4,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
 layer4_1 = l4_1.feedforward(layer4Input)
 layer4_2 = l4_2.feedforward(layer4_1)
-
 layer4Input2 = tf.nn.max_pool(layer4_2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
 layer4_3 = l4_3.feedforward(layer4Input2)
 layer4_4 = l4_4.feedforward(layer4_3,resadd=False)
 
-finalout_reshape = tf.reshape(layer4_4,[batch_size,-1])
-final_soft = tf_soft(finalout_reshape)
+final = tf.reshape(layer4_4,[batch_size,-1])
+final_soft = tf_soft(final)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=finalout_reshape,labels=y))
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=final,labels=y))
 correct_prediction = tf.equal(tf.argmax(final_soft, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # -- auto train ---
 auto_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+
+
+
+
+
+
+
+
 
 # session
 with tf.Session() as sess: 
@@ -155,22 +189,18 @@ with tf.Session() as sess:
 
         # Train Batch
         for current_batch_index in range(0,len(train_batch),batch_size):
-
             current_batch = train_batch[current_batch_index:current_batch_index+batch_size,:,:,:]
             current_batch_label = train_label[current_batch_index:current_batch_index+batch_size,:]
-
-            sess_results = sess.run( [cost,accuracy,correct_prediction,final_soft,finalout_reshape,auto_train], feed_dict= {x:current_batch,y:current_batch_label})
+            sess_results = sess.run([cost,accuracy,correct_prediction,final_soft,final,auto_train], feed_dict= {x:current_batch,y:current_batch_label})
             print("current iter:", iter, ' current batch: ', current_batch_index, " current cost: %.5f"%sess_results[0],' current acc: %.5f'%sess_results[1], end='\r')
             train_total_cost = train_total_cost + sess_results[0]
             train_total_acc = train_total_acc + sess_results[1]
 
-        # Test batch
+        # Test Batch
         for current_batch_index in range(0,len(test_batch),batch_size):
-
             current_batch = test_batch[current_batch_index:current_batch_index+batch_size,:,:,:]
             current_batch_label = test_label[current_batch_index:current_batch_index+batch_size,:]
-
-            sess_results = sess.run( [cost,accuracy,correct_prediction], feed_dict= {x:current_batch,y:current_batch_label})
+            sess_results = sess.run([cost,accuracy,correct_prediction], feed_dict= {x:current_batch,y:current_batch_label})
             print("current iter:", iter, ' current batch: ', current_batch_index, " current cost: %.5f"%sess_results[0],' current acc: %.5f'%sess_results[1], end='\r')
             test_total_cost = test_total_cost + sess_results[0]
             test_total_acc = test_total_acc + sess_results[1]
@@ -178,7 +208,6 @@ with tf.Session() as sess:
         # store
         train_cost_overtime.append(train_total_cost/(len(train_batch)/batch_size ) )
         train_acc_overtime.append(train_total_acc/(len(train_batch)/batch_size ) )
-
         test_cost_overtime.append(test_total_cost/(len(test_batch)/batch_size ) )
         test_acc_overtime.append(test_total_acc/(len(test_batch)/batch_size ) )
         
@@ -191,8 +220,6 @@ with tf.Session() as sess:
             print("Avg Test Acc: ", test_acc_overtime[-1])
             print('-----------')      
         train_total_cost,train_total_acc,test_total_cost,test_total_acc=0,0,0,0            
-                
-        
 
 # plot and save
 plt.figure()
