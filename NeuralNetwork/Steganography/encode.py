a5454c0f9f51fe04f057e11d3ab917b048e27b03
Jae <jae.duk.seo@ryerson.ca>
2018-04-28 14:02:47 -0400

    added files

--- NeuralNetwork/Steganography/encode.py
+++ NeuralNetwork/Steganography/encode.py
@@ -1,6 +1,108 @@
 import base64
+import tensorflow as tf
+import numpy as np,sys,os
+from numpy import float32
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from scipy.ndimage import imread
+from scipy.misc import imresize
+import hashlib
+np.random.seed(678)
+tf.set_random_seed(678)
 
+# Activation Functions - however there was no indication in the original paper
+def tf_Relu(x): return tf.nn.relu(x)
+def d_tf_Relu(x): return tf.cast(tf.greater(x,0),tf.float32)
 
+def tf_log(x): return tf.sigmoid(x)
+def d_tf_log(x): return tf_log(x) * (1.0 - tf.log(x))
 
+def tf_tanh(x): return tf.tanh(x)
+def d_tf_tanh(x): return 1.0 - tf.square(tf_tanh(x))
+
+# make class 
+class CNNLayer():
+    
+    def __init__(self,ker,in_c,out_c,act,d_act,):
+        
+        self.w = tf.Variable(tf.truncated_normal([ker,ker,in_c,out_c],stddev=0.005))
+        self.act,self.d_act = act,d_act
+        self.m,self.v = tf.Variable(tf.zeros_like(self.w)),tf.Variable(tf.zeros_like(self.w))
+
+    def feedforward(self,input,stride=1):
+        self.input  = input
+        self.layer  = tf.nn.conv2d(input,self.w,strides = [1,stride,stride,1],padding='SAME')
+        self.layerA = self.act(self.layer)
+        return self.layerA
+
+    def backprop(self,gradient,stride=1):
+        grad_part_1 = gradient
+        grad_part_2 = self.d_act(self.layer)
+        grad_part_3 = self.input
+
+        grad_middle = tf.multiply(grad_part_1,grad_part_2)
+        grad = tf.nn.conv2d_backprop_filter(
+            input = grad_part_3,filter_sizes = self.w.shape,
+            out_backprop = grad_middle,strides=[1,1,1,1], padding="SAME"
+        )
+
+        grad_pass  = tf.nn.conv2d_backprop_input(
+            input_sizes=[batch_size] + list(self.input.shape[1:]),filter = self.w ,
+            out_backprop = grad_middle,strides=[1,1,1,1], padding="SAME"
+        )
+
+        update_w = []
+
+        update_w.append(
+            tf.assign( self.m,self.m*beta_1 + (1-beta_1) * grad   )
+        )
+        update_w.append(
+            tf.assign( self.v,self.v*beta_2 + (1-beta_2) * grad ** 2   )
+        )
+
+        m_hat = self.m / (1-beta1)
+        v_hat = self.v / (1-beta2)
+        adam_middel = learning_rate/(tf.sqrt(v_hat) + adam_e)
+        update_w.append(tf.assign(self.w,tf.subtract(self.w,tf.multiply(adam_middel,m_hat))))
+
+        return grad_pass,update_w
+
+data_location = "../../Dataset/Semanticdataset100/image/"
+train_data = []  # create an empty list
+for dirName, subdirList, fileList in sorted(os.walk(data_location)):
+    for filename in fileList:
+        if ".jpg" in filename.lower() :
+            train_data.append(os.path.join(dirName,filename))
+
+data_location =  "../../Dataset/Semanticdataset100/ground-truth/"
+train_data_gt = []  # create an empty list
+for dirName, subdirList, fileList in sorted(os.walk(data_location)):
+    for filename in fileList:
+        if ".png" in filename.lower() :
+            train_data_gt.append(os.path.join(dirName,filename))
+
+train_images = np.zeros(shape=(100,150,150,3))
+train_labels = np.zeros(shape=(100,150,150,3))
+
+for file_index in range(len(train_data)):
+    train_images[file_index,:,:]   = imresize(imread(train_data[file_index],mode='RGB'),(150,150))
+    train_labels[file_index,:,:]   = imresize(imread(train_data_gt[file_index],mode='RGB'),(150,150))
+
+train_images[:,:,:,0]  = (train_images[:,:,:,0] - train_images[:,:,:,0].min(axis=0)) / (train_images[:,:,:,0].max(axis=0) - train_images[:,:,:,0].min(axis=0))
+train_images[:,:,:,1]  = (train_images[:,:,:,1] - train_images[:,:,:,1].min(axis=0)) / (train_images[:,:,:,1].max(axis=0) - train_images[:,:,:,1].min(axis=0))
+train_images[:,:,:,2]  = (train_images[:,:,:,2] - train_images[:,:,:,2].min(axis=0)) / (train_images[:,:,:,2].max(axis=0) - train_images[:,:,:,2].min(axis=0))
+train_labels[:,:,:,0]  = (train_labels[:,:,:,0] - train_labels[:,:,:,0].min(axis=0)) / (train_labels[:,:,:,0].max(axis=0) - train_labels[:,:,:,0].min(axis=0))
+
+X = shuffle(train_images)
+s_images = X[:50,:,:,:]
+c_images = X[50:,:,:,:]
+
+temp = base64.b64encode(s_images[3,:,:,:])
+print(len(temp))
+
+print('------------------------')
+hash_object = hashlib.sha512(temp)
+hex_dig = hash_object.hexdigest()
+print(len(hex_dig))
 
 # -- end code --
