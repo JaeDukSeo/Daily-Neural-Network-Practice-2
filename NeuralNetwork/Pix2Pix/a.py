8c68568d78bca41e3952b9bccd440379d4423f37
Jae <jae.duk.seo@ryerson.ca>
2018-04-29 13:11:44 -0400

    added

--- NeuralNetwork/Pix2Pix/a.py
+++ NeuralNetwork/Pix2Pix/a.py
@@ -1,4 +1,113 @@
+import tensorflow as tf
+import numpy as np,sys,os
+from numpy import float32
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from scipy.ndimage import imread
+from scipy.misc import imresize
 
+np.random.seed(678)
+tf.set_random_seed(678)
+
+# Activation Functions - however there was no indication in the original paper
+def tf_Relu(x): return tf.nn.relu(x)
+def d_tf_Relu(x): return tf.cast(tf.greater(x,0),tf.float32)
+def tf_LRelu(x): return tf.nn.leaky_relu(x)
+def d_tf_LRelu(x): return tf.cast(tf.greater(x,0),tf.float32) + tf.cast(tf.less_equal(x,0),tf.float32) *x* 0.2
+def tf_tanh(x): return tf.nn.tanh(x)
+def d_tf_tanh(x): return 1.0 - tf.square(tf_tanh(x))
+def tf_log(x): return tf.nn.sigmoid(x)
+def d_tf_log(x): return tf_log(x) * (1-tf_log(x))
+
+# convolution layer
+class CNNLayer():
+    
+    def __init__(self,ker,in_c,out_c,act,d_act):
+        self.w = tf.Variable(tf.truncated_normal([ker,ker,in_c,out_c],stddev=0.005))
+        self.act,self.d_act = act,d_act
+        self.m,self.v = tf.Variable(tf.zeros_like(self.w)),tf.Variable(tf.zeros_like(self.w))
+
+    def feedforward(self,input,stride=1):
+        self.input  = input
+        self.layer  = tf.nn.conv2d(input,self.w,strides = [1,stride,stride,1],padding='SAME')
+        self.layerA = self.act(self.layer)
+        return self.layerA
+
+    def backprop(self,gradient,stride=1):
+        grad_part_1 = gradient
+        grad_part_2 = self.d_act(self.layer)
+        grad_part_3 = self.input
+
+        grad_middle = tf.multiply(grad_part_1,grad_part_2)
+        grad = tf.nn.conv2d_backprop_filter(
+            input = grad_part_3,filter_sizes = self.w.shape,
+            out_backprop = grad_middle,strides=[1,1,1,1], padding="SAME"
+        )
+
+        grad_pass  = tf.nn.conv2d_backprop_input(
+            input_sizes=[batch_size] + list(self.input.shape[1:]),filter = self.w ,
+            out_backprop = grad_middle,strides=[1,1,1,1], padding="SAME"
+        )
+
+        update_w = []
+
+        update_w.append(
+            tf.assign( self.m,self.m*beta_1 + (1-beta_1) * grad   )
+        )
+        update_w.append(
+            tf.assign( self.v,self.v*beta_2 + (1-beta_2) * grad ** 2   )
+        )
+
+        m_hat = self.m / (1-beta1)
+        v_hat = self.v / (1-beta2)
+        adam_middel = learning_rate/(tf.sqrt(v_hat) + adam_e)
+        update_w.append(tf.assign(self.w,tf.subtract(self.w,tf.multiply(adam_middel,m_hat))))
+
+        return grad_pass,update_w
+
+class CNNLayer_Up():
+    
+    def __init__(self,ker,in_c,out_c):
+        self.w = tf.Variable(tf.random_normal([ker,ker,in_c,out_c],stddev=0.05))
+
+    def feedforward(self,input,stride=1,dilate=1,output=1):
+        self.input  = input
+
+        current_shape_size = input.shape
+
+        self.layer = tf.nn.conv2d_transpose(input,self.w,
+        output_shape=[batch_size] + [int(current_shape_size[1].value*2),int(current_shape_size[2].value*2),int(current_shape_size[3].value/2)],strides=[1,2,2,1],padding='SAME')
+        self.layerA = tf_relu(self.layer)
+        return self.layerA
+
+# generator ---------
+# encoder (unet)
+gl1 = CNNLayer(4,3,64,tf_LRelu,d_tf_LRelu)
+gl2 = CNNLayer(4,64,128,tf_LRelu,d_tf_LRelu)
+gl3 = CNNLayer(4,128,256,tf_LRelu,d_tf_LRelu)
+gl4 = CNNLayer(4,256,512,tf_LRelu,d_tf_LRelu)
+gl5 = CNNLayer(4,512,512,tf_LRelu,d_tf_LRelu)
+
+# decoder
+gl6 = CNNLayer_Up(4,512,512,tf_Relu,d_tf_Relu)
+gl7 = CNNLayer_Up(4,256,1024,tf_Relu,d_tf_Relu)
+gl8 = CNNLayer_Up(4,128,512,tf_Relu,d_tf_Relu)
+gl9 = CNNLayer_Up(4,64,256,tf_Relu,d_tf_Relu)
+gl10 = CNNLayer_Up(4,64,128,tf_Relu,d_tf_Relu)
+gfinal = CNNLayer(4,64,3,tf_tanh,d_tf_tanh)
+# generator ---------
+
+# discrimator -------
+dl1 = CNNLayer(4,3,64)
+dl2 = CNNLayer(4,64,128)
+dl3 = CNNLayer(4,128,256)
+dl3 = CNNLayer(4,256,512)
+dlfinal = CNNLayer(4,512,1)
+
+
+
+
+# discrimator -------
 
 
 
