7749849baba2b932e426baa064a280759c2409e2
Jae <jae.duk.seo@ryerson.ca>
2018-05-02 15:29:12 -0400

    added more

--- competitions/z_refer.py
+++ competitions/z_refer.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
-import os
+import os,sys
 os.environ['OMP_NUM_THREADS'] = '4'
 import gc
 
@@ -20,38 +20,52 @@ dtypes = {
         }
 print('load train....')
 # we save only day 9
-train_df = pd.read_csv(path+"train.csv", dtype=dtypes, skiprows = range(1, 131886954), usecols=['ip','app','device','os', 'channel', 'click_time', 'is_attributed'], parse_dates=['click_time'])
+# train_df = pd.read_csv(path+"train.csv", dtype=dtypes, skiprows = range(1, 131886954), usecols=['ip','app','device','os', 'channel', 'click_time', 'is_attributed'], parse_dates=['click_time'])
+train_df = pd.read_csv(path+"train.csv", dtype=dtypes,  nrows=100,usecols=['ip','app','device','os', 'channel', 'click_time', 'is_attributed'], 
+                        parse_dates=['click_time'])
 print('load test....')
-test_df = pd.read_csv(path+"test.csv", dtype=dtypes, usecols=['ip','app','device','os', 'channel', 'click_time', 'click_id'], parse_dates=['click_time'])
+test_df = pd.read_csv(path+"test.csv", dtype=dtypes, nrows=100,usecols=['ip','app','device','os', 'channel', 'click_time', 'click_id'], 
+                        parse_dates=['click_time'])
+
+# Def: Get the original Training Data Length and append test data
 len_train = len(train_df)
 train_df=train_df.append(test_df)
 del test_df; gc.collect()
+
 
 print('click time....')
 train_df['click_time'] = (train_df['click_time'].astype(np.int64) // 10 ** 9).astype(np.int32)
 train_df['next_click'] = (train_df.groupby(['ip', 'app', 'device', 'os']).click_time.shift(-1) - train_df.click_time).astype(np.float32)
 train_df['next_click'].fillna((train_df['next_click'].mean()), inplace=True)
 
+print(train_df.shape)
+sys.exit()
+
 print('hour, day, wday....')
 train_df['hour'] = pd.to_datetime(train_df.click_time).dt.hour.astype('uint8')
 train_df['day'] = pd.to_datetime(train_df.click_time).dt.day.astype('uint8')
 train_df['wday']  = pd.to_datetime(train_df.click_time).dt.dayofweek.astype('uint8')
+
 print('grouping by ip-day-hour combination....')
 gp = train_df[['ip','day','hour','channel']].groupby(by=['ip','day','hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'qty'})
 train_df = train_df.merge(gp, on=['ip','day','hour'], how='left')
 del gp; gc.collect()
+
 print('group by ip-app combination....')
 gp = train_df[['ip','app', 'channel']].groupby(by=['ip', 'app'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'ip_app_count'})
 train_df = train_df.merge(gp, on=['ip','app'], how='left')
 del gp; gc.collect()
+
 print('group by ip-app-os combination....')
 gp = train_df[['ip','app', 'os', 'channel']].groupby(by=['ip', 'app', 'os'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'ip_app_os_count'})
 train_df = train_df.merge(gp, on=['ip','app', 'os'], how='left')
 del gp; gc.collect()
+
 print("vars and data type....")
 train_df['qty'] = train_df['qty'].astype('uint16')
 train_df['ip_app_count'] = train_df['ip_app_count'].astype('uint16')
 train_df['ip_app_os_count'] = train_df['ip_app_os_count'].astype('uint16')
+
 print("label encoding....")
 from sklearn.preprocessing import LabelEncoder
 train_df[['app','device','os', 'channel', 'hour', 'day', 'wday']].apply(LabelEncoder().fit_transform)
@@ -60,13 +74,6 @@ test_df = train_df[len_train:]
 train_df = train_df[:len_train]
 y_train = train_df['is_attributed'].values
 train_df.drop(['click_id', 'click_time','ip','is_attributed'],1,inplace=True)
-
-print ('neural network....')
-from keras.layers import Input, Embedding, Dense, Flatten, Dropout, concatenate
-from keras.layers import BatchNormalization, SpatialDropout1D, Conv1D
-from keras.callbacks import Callback
-from keras.models import Model
-from keras.optimizers import Adam
 
 max_app = np.max([train_df['app'].max(), test_df['app'].max()])+1
 max_ch = np.max([train_df['channel'].max(), test_df['channel'].max()])+1
@@ -94,7 +101,15 @@ def get_keras_data(dataset):
     }
     return X
 train_df = get_keras_data(train_df)
+print(train_df)
+sys.exit()
 
+print ('neural network....')
+from keras.layers import Input, Embedding, Dense, Flatten, Dropout, concatenate
+from keras.layers import BatchNormalization, SpatialDropout1D, Conv1D
+from keras.callbacks import Callback
+from keras.models import Model
+from keras.optimizers import Adam
 
 
 
