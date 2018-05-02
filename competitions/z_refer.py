ac4bda36ade029865d924d5610e7917de804c8c1
Jae <jae.duk.seo@ryerson.ca>
2018-05-02 14:46:44 -0400

    added reference

--- competitions/z_refer.py
+++ competitions/z_refer.py
@@ -1,24 +1,14 @@
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from sklearn.model_selection import train_test_split # for validation 
-import lightgbm as lgb
-import gc # memory 
-from datetime import datetime # train time checking
+print ('Good luck')
 
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+import os
+os.environ['OMP_NUM_THREADS'] = '4'
+import gc
 
-
-start = datetime.now()
-VALIDATE = False
-RANDOM_STATE = 50
-VALID_SIZE = 0.90
-MAX_ROUNDS = 1000
-EARLY_STOP = 50
-OPT_ROUNDS = 650
-skiprows = range(1,109903891)
-nrows = 75000000
-output_filename = 'submission.csv'
-
-path = '../input/'
-
+path = '../Dataset/TalkingDataAdTracking/'
 dtypes = {
         'ip'            : 'uint32',
         'app'           : 'uint16',
@@ -28,167 +18,150 @@ dtypes = {
         'is_attributed' : 'uint8',
         'click_id'      : 'uint32'
         }
-
-
-
-train_cols = ['ip','app','device','os', 'channel', 'click_time', 'is_attributed']
-train_df = pd.read_csv(path+"train.csv", skiprows=skiprows, nrows=nrows,dtype=dtypes, usecols=train_cols)
-
+print('load train....')
+# we save only day 9
+train_df = pd.read_csv(path+"train.csv", dtype=dtypes, skiprows = range(1, 131886954), usecols=['ip','app','device','os', 'channel', 'click_time', 'is_attributed'], parse_dates=['click_time'])
+print('load test....')
+test_df = pd.read_csv(path+"test.csv", dtype=dtypes, usecols=['ip','app','device','os', 'channel', 'click_time', 'click_id'], parse_dates=['click_time'])
 len_train = len(train_df)
-gc.collect()
+train_df=train_df.append(test_df)
+del test_df; gc.collect()
 
-most_freq_hours_in_test_data = [4, 5, 9, 10, 13, 14]
-least_freq_hours_in_test_data = [6, 11, 15]
+print('click time....')
+train_df['click_time'] = (train_df['click_time'].astype(np.int64) // 10 ** 9).astype(np.int32)
+train_df['next_click'] = (train_df.groupby(['ip', 'app', 'device', 'os']).click_time.shift(-1) - train_df.click_time).astype(np.float32)
+train_df['next_click'].fillna((train_df['next_click'].mean()), inplace=True)
 
-def prep_data( df ):
-    
-    df['hour'] = pd.to_datetime(df.click_time).dt.hour.astype('uint8')
-    df['day'] = pd.to_datetime(df.click_time).dt.day.astype('uint8')
-    df.drop(['click_time'], axis=1, inplace=True)
-    gc.collect()
-    
-    df['in_test_hh'] = (   3 
-                         - 2*df['hour'].isin(  most_freq_hours_in_test_data ) 
-                         - 1*df['hour'].isin( least_freq_hours_in_test_data ) ).astype('uint8')
-    gp = df[['ip', 'day', 'in_test_hh', 'channel']].groupby(by=['ip', 'day', 'in_test_hh'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_day_test_hh'})
-    df = df.merge(gp, on=['ip','day','in_test_hh'], how='left')
-    df.drop(['in_test_hh'], axis=1, inplace=True)
-    df['nip_day_test_hh'] = df['nip_day_test_hh'].astype('uint32')
-    del gp
-    gc.collect()
+print('hour, day, wday....')
+train_df['hour'] = pd.to_datetime(train_df.click_time).dt.hour.astype('uint8')
+train_df['day'] = pd.to_datetime(train_df.click_time).dt.day.astype('uint8')
+train_df['wday']  = pd.to_datetime(train_df.click_time).dt.dayofweek.astype('uint8')
+print('grouping by ip-day-hour combination....')
+gp = train_df[['ip','day','hour','channel']].groupby(by=['ip','day','hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'qty'})
+train_df = train_df.merge(gp, on=['ip','day','hour'], how='left')
+del gp; gc.collect()
+print('group by ip-app combination....')
+gp = train_df[['ip','app', 'channel']].groupby(by=['ip', 'app'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'ip_app_count'})
+train_df = train_df.merge(gp, on=['ip','app'], how='left')
+del gp; gc.collect()
+print('group by ip-app-os combination....')
+gp = train_df[['ip','app', 'os', 'channel']].groupby(by=['ip', 'app', 'os'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'ip_app_os_count'})
+train_df = train_df.merge(gp, on=['ip','app', 'os'], how='left')
+del gp; gc.collect()
+print("vars and data type....")
+train_df['qty'] = train_df['qty'].astype('uint16')
+train_df['ip_app_count'] = train_df['ip_app_count'].astype('uint16')
+train_df['ip_app_os_count'] = train_df['ip_app_os_count'].astype('uint16')
+print("label encoding....")
+from sklearn.preprocessing import LabelEncoder
+train_df[['app','device','os', 'channel', 'hour', 'day', 'wday']].apply(LabelEncoder().fit_transform)
+print ('final part of preparation....')
+test_df = train_df[len_train:]
+train_df = train_df[:len_train]
+y_train = train_df['is_attributed'].values
+train_df.drop(['click_id', 'click_time','ip','is_attributed'],1,inplace=True)
 
-    gp = df[['ip', 'day', 'hour', 'channel']].groupby(by=['ip', 'day', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_day_hh'})
-    df = df.merge(gp, on=['ip','day','hour'], how='left')
-    df['nip_day_hh'] = df['nip_day_hh'].astype('uint16')
-    del gp
-    gc.collect()
-    
-    gp = df[['ip', 'os', 'hour', 'channel']].groupby(by=['ip', 'os', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_os'})
-    df = df.merge(gp, on=['ip','os','hour'], how='left')
-    df['nip_hh_os'] = df['nip_hh_os'].astype('uint16')
-    del gp
-    gc.collect()
+print ('neural network....')
+from keras.layers import Input, Embedding, Dense, Flatten, Dropout, concatenate
+from keras.layers import BatchNormalization, SpatialDropout1D, Conv1D
+from keras.callbacks import Callback
+from keras.models import Model
+from keras.optimizers import Adam
 
-    gp = df[['ip', 'app', 'hour', 'channel']].groupby(by=['ip', 'app',  'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_app'})
-    df = df.merge(gp, on=['ip','app','hour'], how='left')
-    df['nip_hh_app'] = df['nip_hh_app'].astype('uint16')
-    del gp
-    gc.collect()
-
-    gp = df[['ip', 'device', 'hour', 'channel']].groupby(by=['ip', 'device', 'hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'nip_hh_dev'})
-    df = df.merge(gp, on=['ip','device','hour'], how='left')
-    df['nip_hh_dev'] = df['nip_hh_dev'].astype('uint32')
-    del gp
-    gc.collect()
-
-    df.drop( ['ip','day'], axis=1, inplace=True )
-    gc.collect()
-    return df
-
-train_df = prep_data(train_df)
-gc.collect()
-
-params = {
-          'boosting_type': 'gbdt',
-          'objective': 'binary',
-          'metric':'auc',
-          'learning_rate': 0.1,
-          'num_leaves': 9,  # we should let it be smaller than 2^(max_depth)
-          'max_depth': 5,  # -1 means no limit
-          'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
-          'max_bin': 100,  # Number of bucketed bin for feature values
-          'subsample': 0.9,  # Subsample ratio of the training instance.
-          'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
-          'colsample_bytree': 0.7,  # Subsample ratio of columns when constructing each tree.
-          'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
-          'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
-          'nthread': 8,
-          'verbose': 0,
-          'scale_pos_weight':99.7, # because training data is extremely unbalanced 
-         }
-
-target = 'is_attributed'
-predictors = ['app','device','os', 'channel', 'hour', 'nip_day_test_hh', 'nip_day_hh', 'nip_hh_os', 'nip_hh_app', 'nip_hh_dev']
-categorical = ['app', 'device', 'os', 'channel', 'hour']
+max_app = np.max([train_df['app'].max(), test_df['app'].max()])+1
+max_ch = np.max([train_df['channel'].max(), test_df['channel'].max()])+1
+max_dev = np.max([train_df['device'].max(), test_df['device'].max()])+1
+max_os = np.max([train_df['os'].max(), test_df['os'].max()])+1
+max_h = np.max([train_df['hour'].max(), test_df['hour'].max()])+1
+max_d = np.max([train_df['day'].max(), test_df['day'].max()])+1
+max_wd = np.max([train_df['wday'].max(), test_df['wday'].max()])+1
+max_qty = np.max([train_df['qty'].max(), test_df['qty'].max()])+1
+max_c1 = np.max([train_df['ip_app_count'].max(), test_df['ip_app_count'].max()])+1
+max_c2 = np.max([train_df['ip_app_os_count'].max(), test_df['ip_app_os_count'].max()])+1
+def get_keras_data(dataset):
+    X = {
+        'app': np.array(dataset.app),
+        'ch': np.array(dataset.channel),
+        'dev': np.array(dataset.device),
+        'os': np.array(dataset.os),
+        'h': np.array(dataset.hour),
+        'd': np.array(dataset.day),
+        'wd': np.array(dataset.wday),
+        'qty': np.array(dataset.qty),
+        'c1': np.array(dataset.ip_app_count),
+        'c2': np.array(dataset.ip_app_os_count), 
+        'nc': np.array(dataset.next_click)
+    }
+    return X
+train_df = get_keras_data(train_df)
 
 
-if VALIDATE:
 
-    train_df, val_df = train_test_split(train_df, test_size=VALID_SIZE, random_state=RANDOM_STATE, shuffle=True )
-    dtrain = lgb.Dataset(train_df[predictors].values, 
-                         label=train_df[target].values,
-                         feature_name=predictors,
-                         categorical_feature=categorical)
-    del train_df
-    gc.collect()
 
-    dvalid = lgb.Dataset(val_df[predictors].values,
-                         label=val_df[target].values,
-                         feature_name=predictors,
-                         categorical_feature=categorical)
-    del val_df
-    gc.collect()
 
-    evals_results = {}
+emb_n = 50
+dense_n = 1000
+in_app = Input(shape=[1], name = 'app')
+emb_app = Embedding(max_app, emb_n)(in_app)
+in_ch = Input(shape=[1], name = 'ch')
+emb_ch = Embedding(max_ch, emb_n)(in_ch)
+in_dev = Input(shape=[1], name = 'dev')
+emb_dev = Embedding(max_dev, emb_n)(in_dev)
+in_os = Input(shape=[1], name = 'os')
+emb_os = Embedding(max_os, emb_n)(in_os)
+in_h = Input(shape=[1], name = 'h')
+emb_h = Embedding(max_h, emb_n)(in_h) 
+in_d = Input(shape=[1], name = 'd')
+emb_d = Embedding(max_d, emb_n)(in_d) 
+in_wd = Input(shape=[1], name = 'wd')
+emb_wd = Embedding(max_wd, emb_n)(in_wd) 
+in_qty = Input(shape=[1], name = 'qty')
+emb_qty = Embedding(max_qty, emb_n)(in_qty) 
+in_c1 = Input(shape=[1], name = 'c1')
+emb_c1 = Embedding(max_c1, emb_n)(in_c1) 
+in_c2 = Input(shape=[1], name = 'c2')
+emb_c2 = Embedding(max_c2, emb_n)(in_c2) 
+in_nc = Input(shape=[1], name = 'nc')
+fe = concatenate([(emb_app), (emb_ch), (emb_dev), (emb_os), (emb_h), (emb_d), (emb_wd), (emb_qty), (emb_c1), (emb_c2)])
 
-    model = lgb.train(params, 
-                      dtrain, 
-                      valid_sets=[dtrain, dvalid], 
-                      valid_names=['train','valid'], 
-                      evals_result=evals_results, 
-                      num_boost_round=MAX_ROUNDS,
-                      early_stopping_rounds=EARLY_STOP,
-                      verbose_eval=50, 
-                      feval=None)
+# model is here
+s_dout = SpatialDropout1D(0.2)(fe)
+conv = Conv1D(100, kernel_size=4, strides=1, padding='same')(s_dout)
+concat = concatenate([Flatten()(s_dout), Flatten()(conv), (in_nc)])
+x = Dropout(0.2)(Dense(dense_n,activation='relu')(concat))
+x = Dropout(0.2)(Dense(dense_n,activation='relu')(x))
+outp = Dense(1,activation='sigmoid')(x)
+model = Model(inputs=[in_app,in_ch,in_dev,in_os,in_h,in_d,in_wd,in_qty,in_c1,in_c2,in_nc], outputs=outp)
 
-    del dvalid
+# Training starts here
+batch_size = 50000
+epochs = 2
+exp_decay = lambda init, fin, steps: (init/fin)**(1/(steps-1)) - 1
+steps = int(len(list(train_df)[0]) / batch_size) * epochs
+lr_init, lr_fin = 0.002, 0.0002
+lr_decay = exp_decay(lr_init, lr_fin, steps)
+optimizer_adam = Adam(lr=0.002, decay=lr_decay)
+model.compile(loss='binary_crossentropy',optimizer=optimizer_adam,metrics=['accuracy'])
 
-else:
+model.summary()
 
-    gc.collect()
-    dtrain = lgb.Dataset(train_df[predictors].values, label=train_df[target].values,
-                          feature_name=predictors,
-                          categorical_feature=categorical
-                          )
-    del train_df
-    gc.collect()
-
-    evals_results = {}
-
-    model = lgb.train(params, 
-                      dtrain, 
-                      valid_sets=[dtrain], 
-                      valid_names=['train'], 
-                      evals_result=evals_results, 
-                      num_boost_round=OPT_ROUNDS,
-                      verbose_eval=50,
-                      feval=None)
-    
-del dtrain
-gc.collect()
-
-test_cols = ['ip','app','device','os', 'channel', 'click_time', 'click_id']
-test_df = pd.read_csv(path+"test.csv", dtype=dtypes, usecols=test_cols)
-test_df = prep_data(test_df)
-gc.collect()
+class_weight = {0:.01,1:.99} # magic
+model.fit(train_df, y_train, batch_size=batch_size, epochs=2, class_weight=class_weight, shuffle=True, verbose=2)
+del train_df, y_train; gc.collect()
+model.save_weights('imbalanced_data.h5')
 
 sub = pd.DataFrame()
-sub['click_id'] = test_df['click_id']
-sub['is_attributed'] = model.predict(test_df[predictors])
-sub.to_csv(output_filename, index=False, float_format='%.9f')
+sub['click_id'] = test_df['click_id'].astype('int')
+test_df.drop(['click_id', 'click_time','ip','is_attributed'],1,inplace=True)
+test_df = get_keras_data(test_df)
 
-print('=='*35)
-print('============================ Final Report ============================')
-print('=='*35)
-print(datetime.now(), '\n')
-print('{:^17} : {:}'.format('train time', datetime.now()-start))
-print('{:^17} : {:}'.format('output file', output_filename))
-print('{:^17} : {:.5f}'.format('train auc', model.best_score['train']['auc']))
-if VALIDATE:
-    print('{:^17} : {:.5f}\n'.format('valid auc', model.best_score['valid']['auc']))
-    print('{:^17} : {:}\n{:^17} : {}\n{:^17} : {}'.format('VALIDATE', VALIDATE, 'VALID_SIZE', VALID_SIZE, 'RANDOM_STATE', RANDOM_STATE))
-print('{:^17} : {:}\n{:^17} : {}\n{:^17} : {}\n'.format('MAX_ROUNDS', MAX_ROUNDS, 'EARLY_STOP', EARLY_STOP, 'OPT_ROUNDS', model.best_iteration))
-print('{:^17} : {:}\n{:^17} : {}\n'.format('skiprows', skiprows, 'nrows', nrows))
-print('{:^17} : {:}\n{:^17} : {}\n'.format('variables', predictors, 'categorical', categorical))
-print('{:^17} : {:}\n'.format('model params', params))
-print('=='*35)
+print("predicting....")
+sub['is_attributed'] = model.predict(test_df, batch_size=batch_size, verbose=2)
+del test_df; gc.collect()
+print("writing....")
+sub.to_csv('imbalanced_data.csv',index=False)
+
+
+
+
+# -- end code --
