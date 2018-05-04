8f2f363cfa706cce736e94f98e31f7f006f9c788
Jae <jae.duk.seo@ryerson.ca>
2018-05-04 14:55:36 -0400

    added mroe code

--- NeuralNetwork/HowtoLoad/a.py
+++ NeuralNetwork/HowtoLoad/a.py
@@ -29,14 +29,9 @@ for dirName, subdirList, fileList in sorted(os.walk(data_location)):
 train_images = np.zeros(shape=(10,128,128,3))
 train_labels = np.zeros(shape=(10,128,128,3))
 
-
-
 for file_index in range(10):
-    train_images[file_index,:,:]   = imresize(imread(train_data[file_index],mode='RGB'),(128,128))
-    train_labels[file_index,:,:]   = imresize(imread(train_data_gt[file_index],mode='RGB'),(128,128))
-
-train_images = cv.normalize(img,  normalizedImg, 0, 255, cv.NORM_MINMAX)
-train_labels = cv.normalize(img,  normalizedImg, 0, 255, cv.NORM_MINMAX)
+    train_images[file_index,:,:]   = imresize(imread(train_data[file_index],mode='RGB'),(128,128)).astype(int)
+    train_labels[file_index,:,:]   = imresize(imread(train_data_gt[file_index],mode='RGB'),(128,128)).astype(int)
 
 for x in range(10):
     plt.imshow(train_images[x,:,:,:])
@@ -44,21 +39,9 @@ for x in range(10):
     plt.imshow(train_labels[x,:,:,:])
     plt.show()
 
-sys.exit()
 
-train_images[:,:,:,0]  = (train_images[:,:,:,0] - train_images[:,:,:,0].min(axis=0)) / (train_images[:,:,:,0].max(axis=0) - train_images[:,:,:,0].min(axis=0))
-train_images[:,:,:,1]  = (train_images[:,:,:,1] - train_images[:,:,:,1].min(axis=0)) / (train_images[:,:,:,1].max(axis=0) - train_images[:,:,:,1].min(axis=0))
-train_images[:,:,:,2]  = (train_images[:,:,:,2] - train_images[:,:,:,2].min(axis=0)) / (train_images[:,:,:,2].max(axis=0) - train_images[:,:,:,2].min(axis=0))
+train_labels_channels = np.zeros(shape=(10,128,128,20))
 
-train_labels[:,:,:,0]  = (train_labels[:,:,:,0] - train_labels[:,:,:,0].min(axis=0)) / (train_labels[:,:,:,0].max(axis=0) - train_labels[:,:,:,0].min(axis=0))
-train_labels[:,:,:,1]  = (train_labels[:,:,:,1] - train_labels[:,:,:,1].min(axis=0)) / (train_labels[:,:,:,1].max(axis=0) - train_labels[:,:,:,1].min(axis=0))
-train_labels[:,:,:,2]  = (train_labels[:,:,:,2] - train_labels[:,:,:,2].min(axis=0)) / (train_labels[:,:,:,2].max(axis=0) - train_labels[:,:,:,2].min(axis=0))
-
-for x in range(10):
-    plt.imshow(train_images[x,:,:,:])
-    plt.show()
-    plt.imshow(train_labels[x,:,:,:])
-    plt.show()
 
 
 # -- end code --
