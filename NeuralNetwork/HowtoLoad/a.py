a861a857a088bfeeed1f6fdf546ff559f3606164
Jae <jae.duk.seo@ryerson.ca>
2018-05-04 13:59:25 -0400

    Added more code

---
+++ NeuralNetwork/HowtoLoad/a.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np,sys,os
+from numpy import float32
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from scipy.ndimage import imread
+from scipy.misc import imresize
+
+np.random.seed(6783)
+tf.set_random_seed(6785)
+
+# data
+data_location = "../../Dataset/salObj/datasets/imgs/pascal/"
+train_data = []  # create an empty list
+for dirName, subdirList, fileList in sorted(os.walk(data_location)):
+    for filename in fileList:
+        if ".jpg" in filename.lower() :
+            train_data.append(os.path.join(dirName,filename))
+
+data_location =  "../../Dataset/salObj/datasets/masks/pascal/"
+train_data_gt = []  # create an empty list
+for dirName, subdirList, fileList in sorted(os.walk(data_location)):
+    for filename in fileList:
+        if ".png" in filename.lower() :
+            train_data_gt.append(os.path.join(dirName,filename))
+
+train_images = np.zeros(shape=(850,128,128,3))
+train_labels = np.zeros(shape=(850,128,128,1))
+
+for file_index in range(len(train_data)-1):
+    train_images[file_index,:,:]   = imresize(imread(train_data[file_index],mode='RGB'),(128,128))
+    train_labels[file_index,:,:]   = np.expand_dims(imresize(imread(train_data_gt[file_index],mode='F',flatten=True),(128,128)),axis=3)
+
+
+
+
+train_images[:,:,:,0]  = (train_images[:,:,:,0] - train_images[:,:,:,0].min(axis=0)) / (train_images[:,:,:,0].max(axis=0) - train_images[:,:,:,0].min(axis=0)+1e-10)
+train_images[:,:,:,1]  = (train_images[:,:,:,1] - train_images[:,:,:,1].min(axis=0)) / (train_images[:,:,:,1].max(axis=0) - train_images[:,:,:,1].min(axis=0)+1e-10)
+train_images[:,:,:,2]  = (train_images[:,:,:,2] - train_images[:,:,:,2].min(axis=0)) / (train_images[:,:,:,2].max(axis=0) - train_images[:,:,:,2].min(axis=0)+1e-10)
+train_labels[:,:,:,0]  = (train_labels[:,:,:,0] - train_labels[:,:,:,0].min(axis=0)) / (train_labels[:,:,:,0].max(axis=0) - train_labels[:,:,:,0].min(axis=0)+1e-10)
+
+
+
+
+# -- end code --
