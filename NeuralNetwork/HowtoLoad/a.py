c647c385104a9edf9b06d2d1a5935b1041a5cf6d
Jae <jae.duk.seo@ryerson.ca>
2018-05-04 14:29:45 -0400

    added more files

--- NeuralNetwork/HowtoLoad/a.py
+++ NeuralNetwork/HowtoLoad/a.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np,sys,os
 from numpy import float32
 import matplotlib.pyplot as plt
@@ -7,39 +7,58 @@ from scipy.ndimage import imread
 from scipy.misc import imresize
 
 np.random.seed(6783)
-tf.set_random_seed(6785)
+# tf.set_random_seed(6785)
 
-# data
-data_location = "../../Dataset/salObj/datasets/imgs/pascal/"
-train_data = []  # create an empty list
-for dirName, subdirList, fileList in sorted(os.walk(data_location)):
-    for filename in fileList:
-        if ".jpg" in filename.lower() :
-            train_data.append(os.path.join(dirName,filename))
-
-data_location =  "../../Dataset/salObj/datasets/masks/pascal/"
+# data 
+data_location =  "../../Dataset/VOC2011/SegmentationClass/"
 train_data_gt = []  # create an empty list
+only_file_name = []
 for dirName, subdirList, fileList in sorted(os.walk(data_location)):
     for filename in fileList:
         if ".png" in filename.lower() :
             train_data_gt.append(os.path.join(dirName,filename))
+            only_file_name.append(filename[:-4])
 
-train_images = np.zeros(shape=(850,128,128,3))
-train_labels = np.zeros(shape=(850,128,128,1))
+data_location = "../../Dataset/VOC2011/JPEGImages/"
+train_data = []  # create an empty list
+for dirName, subdirList, fileList in sorted(os.walk(data_location)):
+    for filename in fileList:
+        if ".jpg" in filename.lower() and filename.lower()[:-4] in  only_file_name:
+            train_data.append(os.path.join(dirName,filename))
 
-for file_index in range(len(train_data)-1):
+train_images = np.zeros(shape=(10,128,128,3))
+train_labels = np.zeros(shape=(10,128,128,3))
+
+
+
+for file_index in range(10):
     train_images[file_index,:,:]   = imresize(imread(train_data[file_index],mode='RGB'),(128,128))
-    train_labels[file_index,:,:]   = np.expand_dims(imresize(imread(train_data_gt[file_index],mode='F',flatten=True),(128,128)),axis=3)
+    train_labels[file_index,:,:]   = imresize(imread(train_data_gt[file_index],mode='RGB'),(128,128))
 
+train_images = cv.normalize(img,  normalizedImg, 0, 255, cv.NORM_MINMAX)
+train_labels = cv.normalize(img,  normalizedImg, 0, 255, cv.NORM_MINMAX)
 
+for x in range(10):
+    plt.imshow(train_images[x,:,:,:])
+    plt.show()
+    plt.imshow(train_labels[x,:,:,:])
+    plt.show()
 
+sys.exit()
 
-train_images[:,:,:,0]  = (train_images[:,:,:,0] - train_images[:,:,:,0].min(axis=0)) / (train_images[:,:,:,0].max(axis=0) - train_images[:,:,:,0].min(axis=0)+1e-10)
-train_images[:,:,:,1]  = (train_images[:,:,:,1] - train_images[:,:,:,1].min(axis=0)) / (train_images[:,:,:,1].max(axis=0) - train_images[:,:,:,1].min(axis=0)+1e-10)
-train_images[:,:,:,2]  = (train_images[:,:,:,2] - train_images[:,:,:,2].min(axis=0)) / (train_images[:,:,:,2].max(axis=0) - train_images[:,:,:,2].min(axis=0)+1e-10)
-train_labels[:,:,:,0]  = (train_labels[:,:,:,0] - train_labels[:,:,:,0].min(axis=0)) / (train_labels[:,:,:,0].max(axis=0) - train_labels[:,:,:,0].min(axis=0)+1e-10)
+train_images[:,:,:,0]  = (train_images[:,:,:,0] - train_images[:,:,:,0].min(axis=0)) / (train_images[:,:,:,0].max(axis=0) - train_images[:,:,:,0].min(axis=0))
+train_images[:,:,:,1]  = (train_images[:,:,:,1] - train_images[:,:,:,1].min(axis=0)) / (train_images[:,:,:,1].max(axis=0) - train_images[:,:,:,1].min(axis=0))
+train_images[:,:,:,2]  = (train_images[:,:,:,2] - train_images[:,:,:,2].min(axis=0)) / (train_images[:,:,:,2].max(axis=0) - train_images[:,:,:,2].min(axis=0))
 
+train_labels[:,:,:,0]  = (train_labels[:,:,:,0] - train_labels[:,:,:,0].min(axis=0)) / (train_labels[:,:,:,0].max(axis=0) - train_labels[:,:,:,0].min(axis=0))
+train_labels[:,:,:,1]  = (train_labels[:,:,:,1] - train_labels[:,:,:,1].min(axis=0)) / (train_labels[:,:,:,1].max(axis=0) - train_labels[:,:,:,1].min(axis=0))
+train_labels[:,:,:,2]  = (train_labels[:,:,:,2] - train_labels[:,:,:,2].min(axis=0)) / (train_labels[:,:,:,2].max(axis=0) - train_labels[:,:,:,2].min(axis=0))
 
+for x in range(10):
+    plt.imshow(train_images[x,:,:,:])
+    plt.show()
+    plt.imshow(train_labels[x,:,:,:])
+    plt.show()
 
 
 # -- end code --
