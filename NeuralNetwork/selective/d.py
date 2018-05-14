f23e0478051e8f01ddd9e79a9b1113d2e3215296
Jae <jae.duk.seo@ryerson.ca>
2018-05-14 05:21:22 -0400

    a

--- NeuralNetwork/selective/d.py
+++ NeuralNetwork/selective/d.py
@@ -9,13 +9,16 @@ from sklearn.preprocessing import OneHotEncoder
 from skimage.transform import resize
 from imgaug import augmenters as iaa
 import imgaug as ia
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 np.random.seed(678)
 tf.set_random_seed(678)
 ia.seed(678)
 
 def tf_elu(x): return tf.nn.elu(x)
-def d_tf_elu(x): return tf.cast(tf.greater(x,0),tf.float32)  + ( tf_elu(tf.cast(tf.less_equal(x,0),tf.float32) * x) + 1.0)
+def d_tf_elu(x): return tf.cast(tf.greater_equal(x,0),tf.float32)  + (tf_elu(tf.cast(tf.less(x,0),tf.float32) * x) + 1.0)
+def tf_relu(x): return tf.nn.relu(x)
+def d_tf_relu(x): return tf.cast(tf.greater_equal(x,0),tf.float32)
 def tf_softmax(x): return tf.nn.softmax(x)
 def unpickle(file):
     import pickle
@@ -31,13 +34,13 @@ def unpickle(file):
 # ===== Rules that I have learned =====
 
 # data aug
-seq = iaa.Sequential([
+seq = iaa.Sequential([  
     iaa.Sometimes(0.1,
         iaa.Affine(
             translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
         )
     ),
-    iaa.Sometimes(0.1,
+    iaa.Sometimes(0.2,
         iaa.Affine(
             rotate=(-25, 25),
         )
@@ -53,21 +56,24 @@ seq = iaa.Sequential([
 # class
 class CNN():
     
-    def __init__(self,k,inc,out):
+    def __init__(self,k,inc,out,act,d_act):
         self.w = tf.Variable(tf.random_normal([k,k,inc,out],stddev=0.05))
         self.m,self.v_prev = tf.Variable(tf.zeros_like(self.w)),tf.Variable(tf.zeros_like(self.w))
         self.v_hat_prev = tf.Variable(tf.zeros_like(self.w))
+        self.act,self.d_act = act,d_act
+
     def getw(self): return self.w
+
     def feedforward(self,input,stride=1,padding='SAME',droprate=1.0):
         self.input  = input
         self.layer  = tf.nn.dropout(tf.nn.conv2d(input,self.w,strides=[1,stride,stride,1],padding=padding) ,droprate)
-        self.layerA = tf_elu(self.layer)
+        self.layerA = self.act(self.layer)
         return self.layerA 
-    def backprop(self,gradient,learning_rate_change,stride=1,padding='SAME',adam=True,reg=False):
-        grad_part_1 = gradient 
-        grad_part_2 = d_tf_elu(self.layer) 
-        grad_part_3 = self.input
 
+    def backprop(self,gradient,learning_rate_change,stride=1,padding='SAME',mom=False,adam=False,awsgrad=False):
+        grad_part_1 = gradient 
+        grad_part_2 = self.d_act(self.layer) 
+        grad_part_3 = self.input
         grad_middle = grad_part_1 * grad_part_2
 
         grad = tf.nn.conv2d_backprop_filter(
@@ -83,12 +89,33 @@ class CNN():
         )
 
         update_w = []
-        update_w.append(
-            tf.assign( self.m,self.m*0.9 + learning_rate_change * (grad)   )
-        )
-        update_w.append(tf.assign(self.w,tf.subtract(self.w,self.m  )) )   
+        if  adam:
+            update_w.append(
+                tf.assign( self.m,self.m*beta1 + (1-beta1) * (grad)   )
+            )
+            update_w.append(
+                tf.assign( self.v_prev,self.v_prev*beta2 + (1-beta2) * (grad ** 2)   )
+            )
+            m_hat = self.m / (1-beta1)
+            v_hat = self.v_prev / (1-beta2)
+            adam_middel = learning_rate_change/(tf.sqrt(v_hat) + adam_e)
+            adam_middel = tf.multiply(adam_middel,m_hat)
+            update_w.append(tf.assign(self.w,tf.subtract(self.w,adam_middel  )) )    
+        if awsgrad:
+            update_w.append(tf.assign( self.m,self.m*beta1 + (1-beta1) * grad   ))
+            v_t = self.v_prev *beta2 + (1-beta2) * grad ** 2 
+            def f1(): return v_t
+            def f2(): return self.v_hat_prev
+            v_max = tf.cond(tf.greater(tf.reduce_sum(v_t), tf.reduce_sum(self.v_hat_prev) ) , true_fn=f1, false_fn=f2)
+            adam_middel = tf.multiply(learning_rate_change/(tf.sqrt(v_max) + adam_e),self.m)
+            update_w.append(tf.assign(self.w,tf.subtract(self.w,adam_middel  )  ))
+            update_w.append(tf.assign( self.v_prev,v_t ))
+            update_w.append(tf.assign( self.v_hat_prev,v_max ))        
+        if mom:
+            update_w.append(tf.assign( self.m,self.m*beta1 + learning_rate* (grad)   ))
+            update_w.append(tf.assign(self.w,tf.subtract(self.w,self.m  )) )    
 
-        return grad_pass,update_w   
+        return grad_pass,update_w  
 # # data
 PathDicom = "../../Dataset/cifar-10-batches-py/"
 lstFilesDCM = []  # create an empty list
@@ -111,10 +138,9 @@ train_label = onehot_encoder.fit_transform(train_label).toarray().astype(np.floa
 test_batch = unpickle(lstFilesDCM[5])[b'data']
 test_label = np.expand_dims(np.array(unpickle(lstFilesDCM[5])[b'labels']),axis=0).T.astype(np.float32)
 test_label = onehot_encoder.fit_transform(test_label).toarray().astype(np.float32)
-# reshape data
 train_batch = np.reshape(train_batch,(len(train_batch),3,32,32))
 test_batch = np.reshape(test_batch,(len(test_batch),3,32,32))
-# rotate data
+# reshape data rotate data
 train_batch = np.rot90(np.rot90(train_batch,1,axes=(1,3)),3,axes=(1,2))
 test_batch = np.rot90(np.rot90(test_batch,1,axes=(1,3)),3,axes=(1,2)).astype(np.float32)
 # standardize Normalize data per channel
@@ -132,28 +158,27 @@ print(test_label.shape)
 num_epoch = 31
 batch_size = 50
 print_size = 1
+beta1,beta2,adam_e = 0.9,0.9,1e-9
 
-learning_rate = 0.01
+learning_rate = 0.0003
 learnind_rate_decay = 0.001
 
-beta1,beta2,adam_e = 0.9,0.9,1e-8
-
-proportion_rate = 0.0001
-decay_rate = 1
+proportion_rate = 0.1
+decay_rate = 100
 
 # define class
 channel_size = 128
-l1 = CNN(3,3,channel_size)
-l2 = CNN(3,channel_size,channel_size)
-l3 = CNN(3,channel_size,channel_size)
+l1 = CNN(3,3,channel_size,tf_elu,d_tf_elu)
+l2 = CNN(3,channel_size,channel_size,tf_elu,d_tf_elu)
+l3 = CNN(3,channel_size,channel_size,tf_elu,d_tf_elu)
 
-l4 = CNN(3,channel_size,channel_size)
-l5 = CNN(3,channel_size,channel_size)
-l6 = CNN(3,channel_size,channel_size)
+l4 = CNN(3,channel_size,channel_size,tf_elu,d_tf_elu)
+l5 = CNN(3,channel_size,channel_size,tf_elu,d_tf_elu)
+l6 = CNN(3,channel_size,channel_size,tf_elu,d_tf_elu)
 
-l7 = CNN(3,channel_size,channel_size)
-l8 = CNN(1,channel_size,channel_size)
-l9 = CNN(1,channel_size,10)
+l7 = CNN(3,channel_size,channel_size,tf_elu,d_tf_elu)
+l8 = CNN(1,channel_size,channel_size,tf_elu,d_tf_elu)
+l9 = CNN(1,channel_size,10,tf_elu,d_tf_elu)
 
 # graph
 x = tf.placeholder(shape=[None,32,32,3],dtype=tf.float32)
@@ -172,13 +197,13 @@ layer1 = l1.feedforward(x,droprate=droprate1)
 layer2 = l2.feedforward(layer1,droprate=droprate2)
 layer3 = l3.feedforward(layer2,stride=2,droprate=droprate3)
 
-layer4 = l4.feedforward(layer3,droprate=droprate1)
+layer4 = l4.feedforward(layer3,droprate=droprate2)
 layer5 = l5.feedforward(layer4,droprate=droprate3)
-layer6 = l6.feedforward(layer5,stride=2,droprate=droprate2)
+layer6 = l6.feedforward(layer5,stride=2,droprate=droprate1)
 
 layer7 = l7.feedforward(layer6,droprate=droprate3)
-layer8 = l8.feedforward(layer7,padding='VALID',droprate=droprate2)
-layer9 = l9.feedforward(layer8,padding='VALID',droprate=droprate1)
+layer8 = l8.feedforward(layer7,padding='VALID',droprate=droprate1)
+layer9 = l9.feedforward(layer8,padding='VALID',droprate=droprate2)
 
 final_global = tf.reduce_mean(layer9,[1,2])
 final_soft = tf_softmax(final_global)
@@ -187,7 +212,28 @@ cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=final_gl
 correct_prediction = tf.equal(tf.argmax(final_soft, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-auto_train = tf.train.MomentumOptimizer(learning_rate=learning_rate_change,momentum=0.9).minimize(cost)
+# ===== auto train =====
+# auto_train = tf.train.MomentumOptimizer(learning_rate=learning_rate_change,momentum=0.9).minimize(cost)
+# auto_train = tf.train.RMSPropOptimizer(learning_rate=learning_rate_change).minimize(cost)
+# auto_train = tf.train.AdamOptimizer(learning_rate=learning_rate_change).minimize(cost)
+# ===== auto train =====
+
+# ===== manual ====
+grad_prepare = tf.reshape(final_soft-y,[batch_size,1,1,10])
+grad9,grad9_up = l9.backprop(grad_prepare,learning_rate_change=learning_rate_change,padding='VALID',awsgrad=True)
+grad8,grad8_up = l8.backprop(grad9,learning_rate_change=learning_rate_change,padding='VALID',adam=True)
+grad7,grad7_up = l7.backprop(grad8,learning_rate_change=learning_rate_change,awsgrad=True)
+
+grad6,grad6_up = l6.backprop(grad7,learning_rate_change=learning_rate_change,stride=2,adam=True)
+grad5,grad5_up = l5.backprop(grad6,learning_rate_change=learning_rate_change,awsgrad=True)
+grad4,grad4_up = l4.backprop(grad5,learning_rate_change=learning_rate_change,adam=True)
+
+grad3,grad3_up = l3.backprop(grad4,learning_rate_change=learning_rate_change,stride=2,awsgrad=True)
+grad2,grad2_up = l2.backprop(grad3,learning_rate_change=learning_rate_change,adam=True)
+grad1,grad1_up = l1.backprop(grad2,learning_rate_change=learning_rate_change,awsgrad=True)
+
+grad_update = grad9_up + grad8_up+ grad7_up + grad6_up + grad5_up + grad4_up + grad3_up + grad2_up + grad1_up
+# ===== manual ====
 
 # sess
 with tf.Session() as sess:
@@ -204,17 +250,17 @@ with tf.Session() as sess:
 
         train_batch,train_label = shuffle(train_batch,train_label)
 
-        random_drop1 = np.random.uniform(low=0.975,high=1.0)
+        random_drop1 = np.random.uniform(low=0.7,high=0.8)
         random_drop2 = np.random.uniform(low=0.8,high=0.9)
-        random_drop3 = np.random.uniform(low=0.8,high=0.9)
+        random_drop3 = np.random.uniform(low=0.9,high=1.0)
 
         for batch_size_index in range(0,len(train_batch),batch_size//2):
             current_batch = train_batch[batch_size_index:batch_size_index+batch_size//2]
             current_batch_label = train_label[batch_size_index:batch_size_index+batch_size//2]
 
             # online data augmentation here and standard normalization
-            images_aug1 = seq.augment_images(current_batch.astype(np.float32))
-            current_batch = np.vstack((current_batch,images_aug1)).astype(np.float32)
+            images_aug = seq.augment_images(current_batch.astype(np.float32))
+            current_batch = np.vstack((current_batch,images_aug)).astype(np.float32)
             current_batch_label = np.vstack((current_batch_label,current_batch_label)).astype(np.float32)
             current_batch[:,:,:,0]  = (current_batch[:,:,:,0] - current_batch[:,:,:,0].mean(axis=0)) / ( current_batch[:,:,:,0].std(axis=0))
             current_batch[:,:,:,1]  = (current_batch[:,:,:,1] - current_batch[:,:,:,1].mean(axis=0)) / ( current_batch[:,:,:,1].std(axis=0))
@@ -222,7 +268,7 @@ with tf.Session() as sess:
             current_batch,current_batch_label  = shuffle(current_batch,current_batch_label)
             # online data augmentation here and standard normalization
 
-            sess_result = sess.run([cost,accuracy,correct_prediction,auto_train],feed_dict={x:current_batch,y:current_batch_label,
+            sess_result = sess.run([cost,accuracy,correct_prediction,grad_update],feed_dict={x:current_batch,y:current_batch_label,
             iter_variable:iter,learning_rate_dynamic:learning_rate,droprate1:random_drop1,droprate2:random_drop2,droprate3:random_drop3})
             print("Current Iter : ",iter, " current batch: ",batch_size_index, ' Current cost: ', sess_result[0],
             ' Current Acc: ', sess_result[1],end='\r')
@@ -233,7 +279,8 @@ with tf.Session() as sess:
             current_batch = test_batch[test_batch_index:test_batch_index+batch_size]
             current_batch_label = test_label[test_batch_index:test_batch_index+batch_size]
             sess_result = sess.run([cost,accuracy,correct_prediction],
-            feed_dict={x:current_batch,y:current_batch_label,iter_variable:iter,learning_rate_dynamic:learning_rate,droprate1:1.0,droprate2:1.0,droprate3:1.0})
+            feed_dict={x:current_batch,y:current_batch_label,iter_variable:iter,learning_rate_dynamic:learning_rate,
+            droprate1:1.0,droprate2:1.0,droprate3:1.0})
             print("Current Iter : ",iter, " current batch: ",test_batch_index, ' Current cost: ', sess_result[0],
             ' Current Acc: ', sess_result[1],end='\r')
             test_acca = sess_result[1] + test_acca
@@ -242,9 +289,12 @@ with tf.Session() as sess:
         if iter % print_size==0:
             print("\n---------- Learning Rate : ", learning_rate * (1.0/(1.0+learnind_rate_decay*iter)) )
             print("Drop 1 : ",random_drop1," Drop 2: ",random_drop2," Drop 3: ",random_drop3)
+            
             print('Train Current cost: ', train_cota/(len(train_batch)/(batch_size//2)),' Current Acc: ', 
             train_acca/(len(train_batch)/(batch_size//2) ),end='\n')
-            print('Test Current cost: ', test_cota/(len(test_batch)/batch_size),' Current Acc: ', test_acca/(len(test_batch)/batch_size),end='\n')
+
+            print('Test Current cost: ', test_cota/(len(test_batch)/batch_size),' Current Acc: ', 
+            test_acca/(len(test_batch)/batch_size),end='\n')
             print("----------")
 
         train_acc.append(train_acca/(len(train_batch)/batch_size))
