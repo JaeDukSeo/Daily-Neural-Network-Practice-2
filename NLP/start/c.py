22ef99133ab16a6af86bd63c2fd4dd277758b042
Jae <jae.duk.seo@ryerson.ca>
2018-05-27 11:10:09 -0400

    'word'

--- NLP/start/c.py
+++ NLP/start/c.py
@@ -1,6 +1,15 @@
+import nltk 
+import numpy as np
+from nltk.tokenize import word_tokenize
 
-
-
+# question 7
+sentence = "The dog was big but it was so doggy and dog"
+word_list = word_tokenize(sentence)
+word_set = set(word_list)
+freq = {}
+for word in word_set:
+    freq[word] = word_list.count(word) 
+    print(word,' : ',freq[word])
 
 
 
