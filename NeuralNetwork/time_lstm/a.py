4b76a71fa33321851bf6e28723ffc8653f56a586
JDSeo <JDSeo@jdseos-mbp.wifi.int.ryerson.ca>
2018-05-31 21:33:57 -0400

    lstm

--- NeuralNetwork/time_lstm/a.py
+++ NeuralNetwork/time_lstm/a.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+import numpy as np,sys
 # import tensorflow as tf
 import matplotlib.pyplot as plt
 import plotly.plotly as py
@@ -27,18 +27,16 @@ a_original_data = df.Mean.values
 
 # 4. Normalize by slide window (Normalize)
 from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
+scaler = MinMaxScaler(feature_range=(0, 1))
 train_data = np.expand_dims(Mean_list[:6273],axis=1)
 test_data =  np.expand_dims(Mean_list[6273:],axis=1)
 
-smoothing_window_size = 2091
-for di in range(0,len(train_data),smoothing_window_size):
-    scaler.fit(train_data[di:di+smoothing_window_size,:])
-    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
-test_data = scaler.fit(test_data).transform(test_data)
-print(train_data.shape)
-print(test_data.shape)
-b_norm_data =  np.concatenate([train_data,test_data],axis=0)
+# smoothing_window_size = 2091
+# for di in range(0,len(train_data),smoothing_window_size):
+#     scaler.fit(train_data[di:di+smoothing_window_size,:])
+#     train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+# test_data = scaler.fit(test_data).transform(test_data)
+# b_norm_data =  np.concatenate([train_data,test_data],axis=0)
 
 # 5. Moving Average to smoothenout the training data (only)
 EMA = 0.0
@@ -48,28 +46,81 @@ for ti in range(len(train_data)):
   train_data[ti,0] = EMA
 c_norm_data_train =  np.concatenate([train_data,test_data],axis=0)
 
-
-# 6. Moving Window Average Method
+# 5. Standard Average of Window
 window_size = 100
-N = train_data.size
-std_avg_predictions = list()
+N = Mean_list.size
+std_avg_predictions = list(Mean_list[:window_size])
 std_avg_x = list()
-mse_errors = list()
+abs_error = list()
 
 for pred_idx in range(window_size,N):
+    std_avg_predictions.append(np.mean(Mean_list[pred_idx-window_size:pred_idx]))
+    abs_error.append( np.abs(std_avg_predictions[-1]-Mean_list[pred_idx]))
+print('MABSE error for standard averaging: %.5f'%(0.5*np.mean(abs_error)))
 
-    print(pred_idx)
-    if pred_idx >= N:
-        date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
-    else:
-        date = df.loc[pred_idx,'Date']
+# plt.figure()
+# plt.plot(Mean_list,color='b',label='True')
+# plt.plot(df.Low,color='g',label='True')
+# plt.plot(df.Mean,color='r',label='True')
+# plt.plot(df.High,color='y',label='True')
+# plt.plot(std_avg_predictions,color='orange',label='Prediction')
+# plt.xlabel('Date')
+# plt.ylabel('Mid Price')
+# plt.legend()
+# plt.show()
 
-    std_avg_predictions.append(np.mean(train_data[pred_idx-window_size:pred_idx]))
-    mse_errors.append((std_avg_predictions[-1]-train_data[pred_idx])**2)
-    std_avg_x.append(date)
-    input()
+# 6. EXP Average of Window
+window_size = 100
+N = Mean_list.size
 
-print('MSE error for standard averaging: %.5f'%(0.5*np.mean(mse_errors)))
+run_avg_predictions = []
+run_avg_x = []
+
+abs_errors = []
+
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
+decay = 0.9
+
+for pred_idx in range(1,N):
+    running_mean = running_mean*decay + (1.0-decay)*Mean_list[pred_idx-1]
+    run_avg_predictions.append(running_mean)
+    abs_errors.append(np.abs(run_avg_predictions[-1]-Mean_list[pred_idx]))
+
+print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(abs_errors)))
+
+# plt.figure()
+# plt.plot(Mean_list,color='b',label='True')
+# plt.plot(df.Low,color='g',label='True')
+# plt.plot(df.Mean,color='r',label='True')
+# plt.plot(df.High,color='y',label='True')
+# plt.plot(run_avg_predictions,color='orange',label='Prediction')
+# plt.xlabel('Date')
+# plt.ylabel('Mid Price')
+# plt.legend()
+# plt.show()
+
+
+
+
+
+# 7. LSTM 
+print(max(Mean_list))
+print(min(Mean_list))
+Mean_list_scale = scaler.fit_transform( np.expand_dims(np.asarray(Mean_list),1))
+print(Mean_list_scale.min())
+print(Mean_list_scale.max())
+
+plt.plot(Mean_list,color='red')
+plt.show()
+
+plt.plot(Mean_list_scale)
+plt.show()
+
+sys.exit()
+
+
+
 
 
 # -- end code --
