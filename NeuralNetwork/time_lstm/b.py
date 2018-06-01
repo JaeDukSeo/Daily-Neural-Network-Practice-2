6faeb66b38b078621bce5ef3e5f4ba6da11ddc17
JDSeo <JDSeo@jdseos-mbp.wifi.int.ryerson.ca>
2018-05-31 23:18:57 -0400

    added

--- NeuralNetwork/time_lstm/b.py
+++ NeuralNetwork/time_lstm/b.py
@@ -9,11 +9,11 @@ import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 from numpy import newaxis
 
-# 0. Get the Data
+# 0. Get the Data and simple sorting and check NaN
 df = pd.read_csv('../../Dataset/aapl.csv',delimiter=',',usecols=['Date','Open','High','Low','Close'])
 df.Date = pd.to_datetime(df.Date)
+print('Is there any Null?  : ',df.isnull().values.any())
 df = df.sort_values('Date')
-
 print(df.head())
 print(df.tail())
 print('------------')
@@ -21,6 +21,40 @@ print(df.info())
 print('------------')
 print(df.describe())
 
-data = [go.Scatter(x=df.Date,y=df.High)]
+# 1. Create the Mean Value and plot
+df['Mean'] = (df.High+df.Low)/2.0
+
+trace1 = go.Scatter(
+    x = df.Date,y = df.Close,
+    name = 'Close',mode='line'
+)
+trace2 = go.Scatter(
+    x = df.Date,y = df.Open,
+    name = 'Open',mode='line'
+)
+trace3 = go.Scatter(
+    x = df.Date,y = df.High,
+    name = 'High',mode='line'
+)
+trace4 = go.Scatter(
+    x = df.Date,y = df.Low,
+    name = 'Low',mode='line'
+)
+trace5 = go.Scatter(
+    x = df.Date,y = df.Mean,
+    name = 'Mean',mode='line'
+)
+data = [trace1,trace2,trace3,trace4,trace5]
 plot(data)
+
+
+
+
+
+
+
+
+
+
+
 # -- end code --
