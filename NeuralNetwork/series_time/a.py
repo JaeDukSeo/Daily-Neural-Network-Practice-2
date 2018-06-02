21d8758955a7d421c7b2027451da8098ddd38a80
JDSeo <JDSeo@jdseos-mbp.wifi.int.ryerson.ca>
2018-06-02 00:18:13 -0400

    sa

--- NeuralNetwork/series_time/a.py
+++ NeuralNetwork/series_time/a.py
@@ -34,8 +34,37 @@ trace4 = go.Scatter(
     x = df.Date,y = df.Mean,
     name = 'Mean Stock Value',mode='line'
 )
-data = [trace1,trace2,trace3,trace4]
-plot(data)
 
+
+# a. Standard Average of Window
+Mean_list = list(df.Mean)
+window_size = 100
+N = Mean_list.size
+std_avg_predictions = list(Mean_list[:window_size])
+for pred_idx in range(window_size,N):
+    std_avg_predictions.append(np.mean(Mean_list[pred_idx-window_size:pred_idx]))
+
+# b. EXP Average of Window
+window_size = 100
+run_avg_predictions = []
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
+decay = 0.9
+
+for pred_idx in range(1,N):
+    running_mean = running_mean*decay + (1.0-decay)*Mean_list[pred_idx-1]
+    run_avg_predictions.append(running_mean)
+
+trace5 = go.Scatter(
+    x = df.Date,y = std_avg_predictions,
+    name = 'Mean Stock Value',mode='line'
+)
+trace6 = go.Scatter(
+    x = df.Date,y = run_avg_predictions,
+    name = 'Mean Stock Value',mode='line'
+)
+
+data = [trace1,trace2,trace3,trace4,trace5,trace6]
+plot(data)
 
 # -- end code --
