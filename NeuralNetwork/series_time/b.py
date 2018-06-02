fb854e4b8e817ba484b79117fcd3c573b3eb98ea
JDSeo <JDSeo@JDSeos-MacBook-Pro.local>
2018-06-02 19:49:48 -0400

    added Graph

--- NeuralNetwork/series_time/b.py
+++ NeuralNetwork/series_time/b.py
@@ -66,8 +66,7 @@ ax1 = fig.add_subplot(211)
 fig = sm.graphics.tsa.plot_acf(df.First_Mean.iloc[1:], lags=40, ax=ax1)
 ax2 = fig.add_subplot(212)
 fig = sm.graphics.tsa.plot_pacf(df.First_Mean.iloc[1:], lags=40, ax=ax2)
-
-
+# plt.show()
 
 
 
