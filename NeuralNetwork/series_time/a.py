79fbf42a4d67db2a8deefa67a1147bdf68cee82d
JDSeo <JDSeo@jdseos-mbp.wifi.int.ryerson.ca>
2018-06-01 23:47:25 -0400

    tme2

--- NeuralNetwork/series_time/a.py
+++ NeuralNetwork/series_time/a.py
@@ -12,9 +12,30 @@ from numpy import newaxis
 # 0. Get the Data and simple sorting and check NaN
 df = pd.read_csv('../../Dataset/aapl.csv',delimiter=',',usecols=['Date','Open','High','Low','Close'])
 df.Date = pd.to_datetime(df.Date)
+df['Mean'] = (df.High + df.Low )/2.0
 
-print(df.head())
 
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+decomposition = seasonal_decompose(df.Mean, freq=365) 
+trace1 = go.Scatter(
+    x = df.Date,y = decomposition.trend,
+    name = 'Trend',mode='line'
+)
+trace2 = go.Scatter(
+    x = df.Date,y = decomposition.seasonal,
+    name = 'Seasonal',mode='line'
+)
+trace3 = go.Scatter(
+    x = df.Date,y = decomposition.resid,
+    name = 'Residual',mode='line'
+)
+trace4 = go.Scatter(
+    x = df.Date,y = df.Mean,
+    name = 'Mean Stock Value',mode='line'
+)
+data = [trace1,trace2,trace3,trace4]
+plot(data)
 
 
 # -- end code --
