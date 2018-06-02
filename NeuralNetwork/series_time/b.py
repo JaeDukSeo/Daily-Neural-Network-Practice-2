eaa682e6a92a3785cc6cd24154c3797030bee750
JDSeo <JDSeo@JDSeos-MacBook-Pro.local>
2018-06-02 19:48:48 -0400

    time

--- NeuralNetwork/series_time/b.py
+++ NeuralNetwork/series_time/b.py
@@ -15,6 +15,93 @@ df.Date = pd.to_datetime(df.Date)
 df['Mean'] = (df.High + df.Low )/2.0
 
 
+
+
+
+from statsmodels.tsa.stattools import adfuller
+def test_stationarity(timeseries):
+
+    #Determing rolling statistics mean and std
+    rolmean = timeseries.rolling(window=30,center=False).mean()
+    rolstd = timeseries.rolling(window=30,center=False).std()
+
+    # Plot the scatter variable
+    trace1 = go.Scatter(
+        x = df.Date,y = timeseries,
+        name = 'Original',mode='line'
+    )
+    trace2 = go.Scatter(
+        x = df.Date,y = rolmean,
+        name = 'Rolling Mean',mode='line'
+    )
+    trace3 = go.Scatter(
+        x = df.Date,y = rolstd,
+        name = 'Rolling STD',mode='line'
+    )
+    data = [trace1,trace2,trace3]
+    # plot(data)
+    
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print(dfoutput)
+    return data
+
+# test_stationarity(df.Mean)
+df['First_Mean'] = df.Mean  - df.Mean.shift(1)
+data1 = test_stationarity(df.Mean )
+data2 = test_stationarity(df.First_Mean.dropna())
+# plot(data1 + data2)
+# plot(data2)
+
+import seaborn as sns
+import statsmodels.api as sm  
+from statsmodels.tsa.stattools import acf  
+from statsmodels.tsa.stattools import pacf
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+fig = sm.graphics.tsa.plot_acf(df.First_Mean.iloc[1:], lags=40, ax=ax1)
+ax2 = fig.add_subplot(212)
+fig = sm.graphics.tsa.plot_pacf(df.First_Mean.iloc[1:], lags=40, ax=ax2)
+
+
+
+
+
+
+# -- end code --
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sys.exit()
+# 1. Decomposition
 from statsmodels.tsa.seasonal import seasonal_decompose
 decomposition = seasonal_decompose(df.Mean, freq=365) 
 trace1 = go.Scatter(
@@ -33,7 +120,6 @@ trace4 = go.Scatter(
     x = df.Date,y = df.Mean,
     name = 'Mean Stock Value',mode='line'
 )
-
 
 # a. Standard Average of Window
 Mean_list = list(df.Mean)
@@ -63,9 +149,7 @@ trace6 = go.Scatter(
     name = 'Moving AVG',mode='line'
 )
 
-
-
-
+# c. Auto Regression Model
 from statsmodels.tsa.ar_model import AR
 window_size = 50
 ar_list = list(Mean_list[:window_size])
