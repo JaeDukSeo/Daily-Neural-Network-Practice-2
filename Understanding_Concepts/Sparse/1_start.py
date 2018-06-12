050afae763f6f1e799f067f31aafbb1bfe06472d
Jae <jae.duk.seo@ryerson.ca>
2018-06-12 19:23:12 -0400

    aded

--- Understanding_Concepts/Sparse/1_start.py
+++ Understanding_Concepts/Sparse/1_start.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score
 from scipy.optimize import minimize
 from mpl_toolkits.mplot3d import Axes3D
 
+plt.style.use('ggplot')
 # Principal component analysis
 # Independent Component Analysis.
 # LinearDiscriminantAnalysis
@@ -16,7 +17,7 @@ from sklearn.manifold import TSNE
 
 np.random.seed(678)
 
-# Make Data set
+# Make the Data set
 def load_data_clear_cut():
     X,Y = make_classification(n_samples=100,n_features=3,
                               n_redundant=0,n_clusters_per_class=1,n_classes=3,class_sep=3.2,n_informative=3)
@@ -27,15 +28,15 @@ def load_data_not_so_clear():
                               n_redundant=0,n_clusters_per_class=1,n_classes=3,class_sep=0.2,n_informative=3)
     return X,Y
 
-
+# -------- clear cut difference in data ---------
 # show the original data 
 X,Y = load_data_clear_cut()
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.scatter(X[:, 0], X[:, 1], X[:, 2],marker='o', c=Y)
+plt.title('Original Data Shape : ' + str(X.shape))
 plt.show()
 plt.close('all')
-
 
 print('------- two component ------ ')
 component_number = 2
@@ -63,10 +64,45 @@ plt.scatter(X_tsne[:, 0], X_tsne[:, 1], marker='o', c=Y, edgecolor='k')
 plt.title('TSNE Data Shape : ' + str(X_pca.shape))
 plt.show()
 
-
 # ----------------------------------
 print('------- one component ------ ')
 component_number = 1
+pca = PCA(n_components=component_number)
+X_pca = pca.fit_transform(X)
+plt.scatter(X_pca[:, 0], [1] * len(X_pca), marker='o', c=Y, edgecolor='k')
+plt.title('PCA Data Shape : ' + str(X_pca.shape))
+plt.show()
+
+ica = FastICA(n_components=component_number)
+X_ica = ica.fit_transform(X)
+plt.scatter(X_ica[:, 0], [1] * len(X_pca), marker='o', c=Y, edgecolor='k')
+plt.title('FastICA Data Shape : ' + str(X_pca.shape))
+plt.show()
+
+lda = LinearDiscriminantAnalysis(n_components=component_number)
+X_lda = lda.fit_transform(X,Y)
+plt.scatter(X_lda[:, 0], [1] * len(X_pca), marker='o', c=Y, edgecolor='k')
+plt.title('LinearDiscriminantAnalysis Data Shape : ' + str(X_pca.shape))
+plt.show()
+
+tsne = TSNE(n_components=component_number)
+X_tsne = tsne.fit_transform(X,Y)
+plt.scatter(X_tsne[:, 0], [1] * len(X_pca), marker='o', c=Y, edgecolor='k')
+plt.title('TSNE Data Shape : ' + str(X_pca.shape))
+plt.show()
+# -------- clear cut difference in data ---------
+
+# -------- no clear cut difference in data ---------
+X,Y = load_data_not_so_clear()
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(X[:, 0], X[:, 1], X[:, 2],marker='o', c=Y)
+plt.title('Original Data Shape : ' + str(X.shape))
+plt.show()
+plt.close('all')
+
+print('------- two component ------ ')
+component_number = 2
 pca = PCA(n_components=component_number)
 X_pca = pca.fit_transform(X)
 plt.scatter(X_pca[:, 0], X_pca[:, 1], marker='o', c=Y, edgecolor='k')
@@ -91,6 +127,32 @@ plt.scatter(X_tsne[:, 0], X_tsne[:, 1], marker='o', c=Y, edgecolor='k')
 plt.title('TSNE Data Shape : ' + str(X_pca.shape))
 plt.show()
 
+# ----------------------------------
+print('------- one component ------ ')
+component_number = 1
+pca = PCA(n_components=component_number)
+X_pca = pca.fit_transform(X)
+plt.scatter(X_pca[:, 0], [1] * len(X_pca), marker='o', c=Y, edgecolor='k')
+plt.title('PCA Data Shape : ' + str(X_pca.shape))
+plt.show()
+
+ica = FastICA(n_components=component_number)
+X_ica = ica.fit_transform(X)
+plt.scatter(X_ica[:, 0], [1] * len(X_pca), marker='o', c=Y, edgecolor='k')
+plt.title('FastICA Data Shape : ' + str(X_pca.shape))
+plt.show()
+
+lda = LinearDiscriminantAnalysis(n_components=component_number)
+X_lda = lda.fit_transform(X,Y)
+plt.scatter(X_lda[:, 0], [1] * len(X_pca), marker='o', c=Y, edgecolor='k')
+plt.title('LinearDiscriminantAnalysis Data Shape : ' + str(X_pca.shape))
+plt.show()
+
+tsne = TSNE(n_components=component_number)
+X_tsne = tsne.fit_transform(X,Y)
+plt.scatter(X_tsne[:, 0], [1] * len(X_pca), marker='o', c=Y, edgecolor='k')
+plt.title('TSNE Data Shape : ' + str(X_pca.shape))
+plt.show()
 
 
 # -- end code --
