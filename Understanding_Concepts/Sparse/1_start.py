2f4530a70fc2d56fd8750d59a70932de9e35ec0a
Jae <jae.duk.seo@ryerson.ca>
2018-06-12 18:59:04 -0400

    aded

--- Understanding_Concepts/Sparse/1_start.py
+++ Understanding_Concepts/Sparse/1_start.py
@@ -10,11 +10,13 @@ from mpl_toolkits.mplot3d import Axes3D
 # Independent Component Analysis.
 # LinearDiscriminantAnalysis
 # t-distributed Stochastic Neighbor Embedding
-from sklearn.decomposition import PCA,FastICA,FactorAnalysis
+from sklearn.decomposition import PCA,FastICA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.manifold import TSNE
+
 np.random.seed(678)
 
+# Make Data set
 def load_data_clear_cut():
     X,Y = make_classification(n_samples=100,n_features=3,
                               n_redundant=0,n_clusters_per_class=1,n_classes=3,class_sep=3.2,n_informative=3)
@@ -31,54 +33,63 @@ X,Y = load_data_clear_cut()
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.scatter(X[:, 0], X[:, 1], X[:, 2],marker='o', c=Y)
-# plt.show()
+plt.show()
+plt.close('all')
+
 
 print('------- two component ------ ')
 component_number = 2
 pca = PCA(n_components=component_number)
 X_pca = pca.fit_transform(X)
-print(X_pca.shape)
+plt.scatter(X_pca[:, 0], X_pca[:, 1], marker='o', c=Y, edgecolor='k')
+plt.title('PCA Data Shape : ' + str(X_pca.shape))
+plt.show()
 
 ica = FastICA(n_components=component_number)
 X_ica = ica.fit_transform(X)
-print(X_ica.shape)
+plt.scatter(X_ica[:, 0], X_ica[:, 1], marker='o', c=Y, edgecolor='k')
+plt.title('FastICA Data Shape : ' + str(X_pca.shape))
+plt.show()
 
 lda = LinearDiscriminantAnalysis(n_components=component_number)
 X_lda = lda.fit_transform(X,Y)
-print(X_lda.shape)
+plt.scatter(X_lda[:, 0], X_lda[:, 1], marker='o', c=Y, edgecolor='k')
+plt.title('LinearDiscriminantAnalysis Data Shape : ' + str(X_pca.shape))
+plt.show()
 
 tsne = TSNE(n_components=component_number)
 X_tsne = tsne.fit_transform(X,Y)
-print(X_tsne.shape)
+plt.scatter(X_tsne[:, 0], X_tsne[:, 1], marker='o', c=Y, edgecolor='k')
+plt.title('TSNE Data Shape : ' + str(X_pca.shape))
+plt.show()
 
+
+# ----------------------------------
 print('------- one component ------ ')
 component_number = 1
 pca = PCA(n_components=component_number)
 X_pca = pca.fit_transform(X)
-print(X_pca.shape)
+plt.scatter(X_pca[:, 0], X_pca[:, 1], marker='o', c=Y, edgecolor='k')
+plt.title('PCA Data Shape : ' + str(X_pca.shape))
+plt.show()
 
 ica = FastICA(n_components=component_number)
 X_ica = ica.fit_transform(X)
-print(X_ica.shape)
+plt.scatter(X_ica[:, 0], X_ica[:, 1], marker='o', c=Y, edgecolor='k')
+plt.title('FastICA Data Shape : ' + str(X_pca.shape))
+plt.show()
 
 lda = LinearDiscriminantAnalysis(n_components=component_number)
 X_lda = lda.fit_transform(X,Y)
-print(X_lda.shape)
+plt.scatter(X_lda[:, 0], X_lda[:, 1], marker='o', c=Y, edgecolor='k')
+plt.title('LinearDiscriminantAnalysis Data Shape : ' + str(X_pca.shape))
+plt.show()
 
 tsne = TSNE(n_components=component_number)
 X_tsne = tsne.fit_transform(X,Y)
-print(X_tsne.shape)
-
-# plt.scatter(X_trans[:, 0], X_trans[:, 1], marker='o', c=Y, edgecolor='k')
-# plt.show()
-
-# plt.scatter(X_trans1[:, 0],[5] *len(X_trans1)  ,marker='o', c=Y, edgecolor='k')
-# plt.show()
-
-# print('-----------------------')
-# print('Original Data Shape: ',X.shape)
-# print('New Data Shape: ',X_trans.shape)
-# print('New Data Shape: ',X_trans1.shape)
+plt.scatter(X_tsne[:, 0], X_tsne[:, 1], marker='o', c=Y, edgecolor='k')
+plt.title('TSNE Data Shape : ' + str(X_pca.shape))
+plt.show()
 
 
 
