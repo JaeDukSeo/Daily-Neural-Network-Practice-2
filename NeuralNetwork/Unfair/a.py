f485bc2b83d0f03566e11da92302a8ca3de404d0
Jae <jae.duk.seo@ryerson.ca>
2018-06-17 11:40:33 -0400

    s

--- NeuralNetwork/Unfair/a.py
+++ NeuralNetwork/Unfair/a.py
@@ -254,7 +254,7 @@ learning_rate = 0.00001
 learnind_rate_decay = 0.0
 beta1,beta2,adam_e = 0.9,0.999,1e-8
 
-# define class
+# define class here
 channel_sizes = 200
 l1 = CNN(3,3,channel_sizes,stddev=0.04)
 l2 = CNN(3,channel_sizes,channel_sizes,stddev=0.05)
