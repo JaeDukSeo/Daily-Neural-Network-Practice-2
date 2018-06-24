8ad90943d8ce8c7bab7eebe4a1f06bc709b07b68
Jae <jae.duk.seo@ryerson.ca>
2018-06-24 00:24:37 -0400

    s

---
+++ RRLL/microsoft/m1/a.py
@@ -0,0 +1,13 @@
+import numpy as np
+import sys
+
+if "../" not in sys.path:
+    sys.path.append("../") 
+
+from lib.envs.simple_rooms import SimpleRoomsEnv
+from lib.envs.cliff_walking import CliffWalkingEnv
+from lib.simulation import Experiment
+
+
+
+# -- end code --
