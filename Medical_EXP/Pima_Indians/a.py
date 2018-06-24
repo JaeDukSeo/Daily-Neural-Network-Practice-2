752858d87c264a9291243c87b16cc26a118c06c4
Jae <jae.duk.seo@ryerson.ca>
2018-06-24 03:31:54 -0400

    s

--- Medical_EXP/Pima_Indians/a.py
+++ Medical_EXP/Pima_Indians/a.py
@@ -12,7 +12,18 @@ print(df.dtypes)
 # 2. clustermap heat map
 import seaborn as sns
 sns.set(style="white")
-sns.heatmap(df.corr())
+corr = df.corr()
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
+
+sns.heatmap(corr)
 plt.show()
 
 # -- end code --
