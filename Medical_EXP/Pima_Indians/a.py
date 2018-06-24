deb5c93a6ebc0c5a611e9bc7e99bd0bdf691f9ea
Jae <jae.duk.seo@ryerson.ca>
2018-06-24 03:33:55 -0400

    s

--- Medical_EXP/Pima_Indians/a.py
+++ Medical_EXP/Pima_Indians/a.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(67)
 
 # 0. Read the data
 df = pd.read_csv('diabetes.csv')
@@ -16,14 +19,14 @@ corr = df.corr()
 mask = np.zeros_like(corr, dtype=np.bool)
 mask[np.triu_indices_from(mask)] = True
 # Set up the matplotlib figure
-f, ax = plt.subplots(figsize=(11, 9))
+# f, ax = plt.subplots(figsize=(11, 9))
 # Generate a custom diverging colormap
-cmap = sns.diverging_palette(220, 10, as_cmap=True)
+# cmap = sns.diverging_palette(220, 10, as_cmap=True)
 # Draw the heatmap with the mask and correct aspect ratio
-sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,square=True, linewidths=.5, cbar_kws={"shrink": .5})
-plt.show()
+# sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,square=True, linewidths=.5, cbar_kws={"shrink": .5})
+# plt.show()
 
-sns.heatmap(corr)
+sns.heatmap(corr,mask=mask,square=True)
 plt.show()
 
 # -- end code --
