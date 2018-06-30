78899bb8e54d6485138cf5b07c1613c126a546dc
Jae <jae.duk.seo@ryerson.ca>
2018-06-30 14:25:13 -0400

    s

---
+++ NeuralNetwork/Time_Aware_LSTM/a.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+
+from cryptory import Cryptory
+
+
+# data
+my_cryptory = Cryptory(from_date = "2017-01-01")
+my_cryptory.extract_coinmarketcap("bitcoin")
+
+print(my_cryptor.shape )
+
+
+# -- end code --
