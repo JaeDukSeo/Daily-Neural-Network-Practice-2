5d5b525dd9ad7497668545565a8fc9d165468d16
Jae <jae.duk.seo@ryerson.ca>
2018-07-08 02:01:55 +0900

    s

--- image_caption/reference2/c_load_data.py
+++ image_caption/reference2/c_load_data.py
@@ -177,3 +177,7 @@ filepath = 'model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'
 checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 # fit model
 model.fit([X1train, X2train], ytrain, epochs=20, verbose=2, callbacks=[checkpoint], validation_data=([X1test, X2test], ytest))
+
+
+
+# -- end code --
