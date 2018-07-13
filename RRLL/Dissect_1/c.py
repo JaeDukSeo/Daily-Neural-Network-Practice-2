fe26eb92d3417127f221d9541938ef589bfcdfdc
Jae <jae.duk.seo@ryerson.ca>
2018-07-13 16:22:16 +0900

    s

---
+++ RRLL/Dissect_1/c.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+def return_state_utility(v, T, u, reward, gamma):
+    """Return the state utility.
+
+    @param v the value vector
+    @param T transition matrix
+    @param u utility vector
+    @param reward for that state
+    @param gamma discount factor
+    @return the utility of the state
+    """
+
+    print(T[:,:,0])
+    print(v.shape)
+    sys.exit()
+
+    action_array = np.zeros(4)
+    for action in range(0, 4):
+        action_array[action] = np.sum(np.multiply(u, np.dot(v, T[:,:,action])))
+    return reward + gamma * np.max(action_array)
+
+def main():
+    #Change as you want
+    tot_states = 12
+    gamma = 0.999 #Discount factor
+    iteration = 0 #Iteration counter
+    epsilon = 1e-300 #Stopping criteria small value
+
+    #Transition matrix loaded from file (It is too big to write here)
+    T = np.load("T.npy")
+
+    #Reward vector
+    r = np.array([-0.04, -0.04, -0.04,  +1.0,
+                  -0.04,   0.0, -0.04,  -1.0,
+                  -0.04, -0.04, -0.04, -0.04])    
+
+    #Utility vectors
+    u = np.array([0.0, 0.0, 0.0,  0.0,
+                   0.0, 0.0, 0.0,  0.0,
+                   0.0, 0.0, 0.0,  0.0])
+
+    u1 = np.array([0.0, 0.0, 0.0,  0.0,
+                    0.0, 0.0, 0.0,  0.0,
+                    0.0, 0.0, 0.0,  0.0])
+
+    while True:
+        delta = 0
+        u = u1.copy()
+        iteration += 1
+
+        # Update every state
+        for s in range(tot_states):
+            reward = r[s]
+            v = np.zeros((1,tot_states))
+            v[0,s] = 1.0
+            u1[s] = return_state_utility(v, T, u, reward, gamma)
+            delta = max(delta, np.abs(u1[s] - u[s])) #Stopping criteria     
+        
+        # if stopping area we are going to stop  
+        if delta < epsilon * (1 - gamma) / gamma:
+                print("=================== FINAL RESULT ==================")
+                print("Iterations: " + str(iteration))
+                print("Delta: " + str(delta))
+                print("Gamma: " + str(gamma))
+                print("Epsilon: " + str(epsilon))
+                print("===================================================")
+                print(u[0:4])
+                print(u[4:8])
+                print(u[8:12])
+                print("===================================================")
+                break
+
+if __name__ == "__main__":
+    main()
