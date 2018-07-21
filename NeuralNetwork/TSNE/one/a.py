093116798249a9688dfd5086611ced85ca3763ec
Jae Duk Seo <jae.duk.seo@ryerson.ca>
2018-07-22 02:12:23 +0900

    added

--- NeuralNetwork/TSNE/one/a.py
+++ NeuralNetwork/TSNE/one/a.py
@@ -1,10 +1,12 @@
-import numpy as np
+import numpy as np,sys
 from load_data import load_mnist
+import matplotlib.pyplot as plt
+
 np.random.seed(1)
 
 # Set global parameters
-NUM_POINTS = 200            # Number of samples from MNIST
-CLASSES_TO_USE = [0, 1, 8]  # MNIST classes to use
+NUM_POINTS = 500            # Number of samples from MNIST
+CLASSES_TO_USE = [0, 1, 8,5,3]  # MNIST classes to use
 PERPLEXITY = 20
 SEED = 1                    # Random seed
 MOMENTUM = 0.9
@@ -12,13 +14,6 @@ LEARNING_RATE = 10.
 NUM_ITERS = 500             # Num iterations to train for
 TSNE = False                # If False, Symmetric SNE
 NUM_PLOTS = 5               # Num. times to plot in training
-
-
-
-# Load the first NUM_POINTS 0's, 1's and 8's from MNIST
-X, y = load_mnist('datasets/',
-                      digits_to_keep=CLASSES_TO_USE,
-                      N=NUM_POINTS)
 
 
 def neg_distance(X):
@@ -41,10 +36,16 @@ def calc_prob_matrix(distances, sigmas=None):
     else:
         return softmax_max(distances)
 
-def binary_search(eval_fn, target, tol=1e-10, max_iter=10000, 
-                  lower=1e-20, upper=1000.):
+def perplexity(distances, sigmas):
+    """Wrapper function for quick calculation of 
+    perplexity over a distance matrix."""
+    prob_matrix = calc_prob_matrix(distances, sigmas)
+    entropy = -np.sum(prob_matrix * np.log2(prob_matrix + 1e-10), 1)
+    perplexity = 2.0 ** entropy
+    return perplexity
+
+def binary_search(distance_vec, target, max_iter=20000,tol=1e-13, lower=1e-5, upper=1e5):
     """Perform a binary search over input values to eval_fn.
-    
     # Arguments
         eval_fn: Function that we are optimising over.
         target: Target value we want the function to output.
@@ -57,7 +58,7 @@ def binary_search(eval_fn, target, tol=1e-10, max_iter=10000,
     """
     for i in range(max_iter):
         guess = (lower + upper) / 2.
-        val = eval_fn(guess)
+        val = perplexity(distance_vec,np.array(guess))
         if val > target:
             upper = guess
         else:
@@ -66,43 +67,84 @@ def binary_search(eval_fn, target, tol=1e-10, max_iter=10000,
             break
     return guess
 
-def calc_perplexity(prob_matrix):
-    """Calculate the perplexity of each row 
-    of a matrix of probabilities."""
-    entropy = -np.sum(prob_matrix * np.log2(prob_matrix + 1e-10), 1)
-    perplexity = 2.0 ** entropy
-    return perplexity
-
-def perplexity(distances, sigmas):
-    """Wrapper function for quick calculation of 
-    perplexity over a distance matrix."""
-    return calc_perplexity(calc_prob_matrix(distances, sigmas))
-
 def find_optimal_sigmas(distances, target_perplexity):
     """For each row of distances matrix, find sigma that results
     in target perplexity for that role."""
     sigmas = [] 
     # For each row of the matrix (each point in our dataset)
     for i in range(distances.shape[0]):
-        # Make fn that returns perplexity of this row given sigma
-        eval_fn = lambda sigma: perplexity(distances[i:i+1, :], np.array(sigma))
         # Binary search over sigmas to achieve target perplexity
-        correct_sigma = binary_search(eval_fn, target_perplexity)
+        correct_sigma = binary_search(distances[i:i+1, :], target_perplexity)
         # Append the resulting sigma to our output array
         sigmas.append(correct_sigma)
     return np.array(sigmas)
 
-print('--------')
-temp = np.array([
-    [1,1],
-    [2,2],
-    [3,3]
-])
-temp_distance = neg_distance(temp)
-temp_sigma = find_optimal_sigmas(temp_distance,5)
+def q_joint(Y):
+    """Given low-dimensional representations Y, compute
+    matrix of joint probabilities with entries q_ij."""
+    # Get the distances from every point to every other
+    distances = neg_distance(Y)
+    # Take the elementwise exponent
+    exp_distances = np.exp(distances)
+    # Fill diagonal with zeroes so q_ii = 0
+    np.fill_diagonal(exp_distances, 0.)
+    # Divide by the sum of the entire exponentiated matrix
+    return exp_distances / np.sum(exp_distances).reshape([-1,1])
 
-print('--------')
-X_distance = neg_distance(X)
-X_sigma = find_optimal_sigmas(temp_distance,5)
+def p_conditional_to_joint(P):
+    """Given conditional probabilities matrix P, return
+    approximation of joint distribution probabilities."""
+    return (P + P.T) / (2. * P.shape[0])
+
+def p_joint(X, target_perplexity):
+    """Given a data matrix X, gives joint probabilities matrix.
+
+    # Arguments
+        X: Input data matrix.
+    # Returns:
+        P: Matrix with entries p_ij = joint probabilities.
+    """
+    # Get the negative euclidian distances matrix for our data
+    distances = neg_distance(X)
+    # Find optimal sigma for each row of this distances matrix
+    sigmas = find_optimal_sigmas(distances, target_perplexity)
+    # Calculate the probabilities based on these optimal sigmas
+    p_conditional = calc_prob_matrix(distances, sigmas)
+    # Go from conditional to joint probabilities matrix
+    P = p_conditional_to_joint(p_conditional)
+    return P
+
+def sym_grad(P,Q,Y):
+    """Estimate the gradient of the cost with respect to Y"""
+    pq_diff = P - Q  # NxN matrix
+    pq_expanded = np.expand_dims(pq_diff, 2)  #NxNx1
+    y_diffs = np.expand_dims(Y, 1) - np.expand_dims(Y, 0)  #NxNx2
+    grad = 4. * (pq_expanded * y_diffs).sum(1)  #Nx2
+    return grad
+
+# Set global parameters
+NUM_POINTS = 300            # Number of samples from MNIST
+CLASSES_TO_USE = [0, 1, 8]  # MNIST classes to use
+num_epoch = 1000
+learning_rate = 10
+print_size = 2
+perplexity_number = 15
+
+X, y = load_mnist('datasets/',digits_to_keep=CLASSES_TO_USE,N=NUM_POINTS)
+W = 2.0 * np.random.randn(300,2) + 0.0
+P = p_joint(X,perplexity_number)
+
+for iter in range(num_epoch):
+    Q = q_joint(W)
+    grad = sym_grad(P,Q,W)
+    W = W - learning_rate * grad
+    print('Current Iter: ',iter, ' Current Grad Sum : ',grad.sum(),end='\r')
+    if iter % print_size == 0 : print('\n---------------\n')
+
+plt.figure(figsize=(5,5))
+plt.scatter(W[:,0],W[:,1],c=y)
+plt.axis('off')
+plt.show()
+
 
 # -- end code --
