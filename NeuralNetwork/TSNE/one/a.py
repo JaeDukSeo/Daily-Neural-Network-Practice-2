1f7a27408dc01f5dfaf3b36a1becaddf7cb6f7e3
Jae Duk Seo <jae.duk.seo@ryerson.ca>
2018-07-21 21:15:06 +0900

    TSNE

--- NeuralNetwork/TSNE/one/a.py
+++ NeuralNetwork/TSNE/one/a.py
@@ -20,8 +20,6 @@ X, y = load_mnist('datasets/',
                       digits_to_keep=CLASSES_TO_USE,
                       N=NUM_POINTS)
 
-print(X.shape)
-print(y.shape)
 
 def neg_distance(X):
     X_sum = np.sum(X**2,1)
@@ -39,18 +37,72 @@ def calc_prob_matrix(distances, sigmas=None):
     """Convert a distances matrix to a matrix of probabilities."""
     if sigmas is not None:
         two_sig_sq = 2. * sigmas.reshape([-1, 1]) ** 2
-        return softmax(distances / two_sig_sq)
+        return softmax_max(distances / two_sig_sq)
     else:
-        return softmax(distances)
+        return softmax_max(distances)
 
+def binary_search(eval_fn, target, tol=1e-10, max_iter=10000, 
+                  lower=1e-20, upper=1000.):
+    """Perform a binary search over input values to eval_fn.
+    
+    # Arguments
+        eval_fn: Function that we are optimising over.
+        target: Target value we want the function to output.
+        tol: Float, once our guess is this close to target, stop.
+        max_iter: Integer, maximum num. iterations to search for.
+        lower: Float, lower bound of search range.
+        upper: Float, upper bound of search range.
+    # Returns:
+        Float, best input value to function found during search.
+    """
+    for i in range(max_iter):
+        guess = (lower + upper) / 2.
+        val = eval_fn(guess)
+        if val > target:
+            upper = guess
+        else:
+            lower = guess
+        if np.abs(val - target) <= tol:
+            break
+    return guess
+
+def calc_perplexity(prob_matrix):
+    """Calculate the perplexity of each row 
+    of a matrix of probabilities."""
+    entropy = -np.sum(prob_matrix * np.log2(prob_matrix + 1e-10), 1)
+    perplexity = 2.0 ** entropy
+    return perplexity
+
+def perplexity(distances, sigmas):
+    """Wrapper function for quick calculation of 
+    perplexity over a distance matrix."""
+    return calc_perplexity(calc_prob_matrix(distances, sigmas))
+
+def find_optimal_sigmas(distances, target_perplexity):
+    """For each row of distances matrix, find sigma that results
+    in target perplexity for that role."""
+    sigmas = [] 
+    # For each row of the matrix (each point in our dataset)
+    for i in range(distances.shape[0]):
+        # Make fn that returns perplexity of this row given sigma
+        eval_fn = lambda sigma: perplexity(distances[i:i+1, :], np.array(sigma))
+        # Binary search over sigmas to achieve target perplexity
+        correct_sigma = binary_search(eval_fn, target_perplexity)
+        # Append the resulting sigma to our output array
+        sigmas.append(correct_sigma)
+    return np.array(sigmas)
+
+print('--------')
 temp = np.array([
     [1,1],
     [2,2],
     [3,3]
 ])
-print(neg_distance(temp))
-print(softmax_max(neg_distance(temp)))
+temp_distance = neg_distance(temp)
+temp_sigma = find_optimal_sigmas(temp_distance,5)
 
-
+print('--------')
+X_distance = neg_distance(X)
+X_sigma = find_optimal_sigmas(temp_distance,5)
 
 # -- end code --
