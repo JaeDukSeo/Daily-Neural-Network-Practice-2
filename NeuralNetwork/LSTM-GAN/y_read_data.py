299ddc7a6ac662f187760472a15263896d20516b
Jae <jae.duk.seo@ryerson.ca>
2018-07-25 06:05:16 +0900

    s

--- NeuralNetwork/LSTM-GAN/y_read_data.py
+++ NeuralNetwork/LSTM-GAN/y_read_data.py
@@ -4,12 +4,39 @@ import numpy as np,sys
 from matplotlib import pyplot as plt, cm
 from collections import defaultdict
 
-
-
+from scipy import ndimage
+from PIL import Image, ImageSequence
 
 
 # read the pet data as well as the CT dicom images
-PathDicom = "../../Dataset/LSTM_GAN/RIDER_PHANTOM_PET_CT/"
+PathDicom = "../../Dataset/LSTM_GAN//"
+lstFilesDCM = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom):
+    for filename in fileList:
+        if ".gif" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM.append(os.path.join(dirName,filename))
+
+all_data = []
+for x in range(len(lstFilesDCM)):
+    img  = Image.open(lstFilesDCM[x])
+    frames = np.array([np.array(frame.copy().convert('RGB').getdata(),dtype=np.uint8).reshape(frame.size[1],frame.size[0],3) for frame in ImageSequence.Iterator(img)])
+    all_data.append(frames)
+    print(frames.shape) 
+    print(len(all_data))
+
+# for xx in all_data:
+#     for xxx in xx:
+#         plt.imshow(xxx)
+#         plt.pause(0.3)
+
+
+all_data = np.asarray(all_data)
+print(all_data.shape) 
+
+np.savez('gif.npz', all_data)
+
+sys.exit()
+
 lstFilesDCM = []  # create an empty list os.path.join(dirName,filename)
 pet_scan_images = defaultdict(list);pet_scan_images_count = 0
 ct_scan_images = defaultdict(list);ct_scan_images_count = 0
