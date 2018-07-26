86b7bd8d7085c0d3269170ab46571c8ae1dc4391
Jae <jae.duk.seo@ryerson.ca>
2018-07-27 00:09:33 +0900

    s

--- NeuralNetwork/Sparse_Auto/b_triplt.py
+++ NeuralNetwork/Sparse_Auto/b_triplt.py
@@ -298,15 +298,14 @@ for file_index in range(len(image_list)):
     train_images[file_index,:,:]   = imresize(imread(image_list[file_index],mode='RGB'),(image_resize_px,image_resize_px))
     train_labels[file_index,:,:]   = np.expand_dims(imresize(rgb2gray(imread(mask_list[file_index],mode='RGB')),(image_resize_px,image_resize_px)),3) 
 
-
 train_labels = (train_labels>25.0) * 255.0
 train_images = train_images/255.0
 train_labels = train_labels/255.0
 
-train_batch = train_images[:84]
-train_label = train_labels[:84]
-test_batch = train_images[84:]
-test_label = train_labels[84:]
+train_batch = train_images[:85]
+train_label = train_labels[:85]
+test_batch = train_images[85:]
+test_label = train_labels[85:]
 
 # print out the data shape
 print(train_batch.shape)
@@ -323,39 +322,32 @@ print(test_label.shape)
 print(test_label.max())
 print(test_label.min())
 
-# f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-# for xx in range(len(train_images)):
-#     ax1.imshow(train_images[xx])
-#     ax2.imshow(np.squeeze(train_labels[xx]))
-#     plt.pause(0.5)
-#     plt.cla()
-
 # class
-el1 = CNN(3,3,4)
-el2 = CNN(3,4,8)
-el3 = CNN(3,8,16)
-el4 = CNN(3,16,32)
+el1 = CNN(3,3,8)
+el2 = CNN(3,8,8)
+el3 = CNN(3,8,8)
+el4 = CNN(3,8,8)
 
-reduce_dim = 4
-sparse_layer = Sparse_Filter_Layer(6*6*32,1*1*reduce_dim)
+reduce_dim = 9
+sparse_layer = Sparse_Filter_Layer(6*6*8,1*1*reduce_dim)
 
 dl0 = CNN_Trans(3,4,1)
 dl1 = CNN_Trans(3,4,4)
 fl1 = CNN(3,4,4)
 
-dl2 = CNN_Trans(3,4,20)
+dl2 = CNN_Trans(3,4,12)
 fl2 = CNN(3,4,4)
 
 dl3 = CNN_Trans(3,4,12)
 fl3 = CNN(3,4,4)
 
-dl4 = CNN_Trans(3,4,8)
+dl4 = CNN_Trans(3,4,12)
 fl4 = CNN(3,4,1,act=tf_sigmoid)
 
 # hyper
 num_epoch = 1201
-learning_rate = 0.0005
-batch_size = 1
+learning_rate = 0.0006
+batch_size = 5
 print_size = 100
 
 # graph
@@ -380,9 +372,10 @@ sparse_layer_value1,sparse_cost1 = sparse_layer.feedforward(sparse_layer_input)
 sparse_layer_value2,sparse_cost2 = sparse_layer.feedforward(sparse_layer_input)
 sparse_layer_value3,sparse_cost3 = sparse_layer.feedforward(sparse_layer_input)
 sparse_layer_value4,sparse_cost4 = sparse_layer.feedforward(sparse_layer_input)
+sparse_layer_value5,sparse_cost5 = sparse_layer.feedforward(sparse_layer_input)
+sparse_layer_value1 = sparse_layer_value0 + sparse_layer_value1 + sparse_layer_value2 +sparse_layer_value3+sparse_layer_value4+sparse_layer_value5
 
-sparse_layer_value1 = sparse_layer_value0 + sparse_layer_value1 + sparse_layer_value2 +sparse_layer_value3+sparse_layer_value4
-dlayer0_input = tf.reshape(sparse_layer_value1,[batch_size,2,2,1])
+dlayer0_input = tf.reshape(sparse_layer_value1,[batch_size,3,3,1])
 dlayer0_input = tf.image.resize_images(dlayer0_input, [6, 6],method=tf.image.ResizeMethod.BILINEAR,align_corners=False)
 dlayer0_input2 = tf.cast(dlayer0_input,dtype=tf.float64)
 dlayer0 = dl0.feedforward(dlayer0_input2,stride=1) # 3 3
@@ -394,24 +387,21 @@ flayer1 = fl1.feedforward(dlayer1)
 
 flayer11 = tf.image.resize_images(flayer1, [24, 24],method=tf.image.ResizeMethod.BILINEAR,align_corners=False)
 flayer11 = tf.cast(flayer11,dtype=tf.float64)
-
 dlayer2 = dl2.feedforward(tf.concat([flayer11,elayer3],3),stride=1) # 8 8
 flayer2 = fl2.feedforward(dlayer2)
 
 flayer21 = tf.image.resize_images(flayer2, [48, 48],method=tf.image.ResizeMethod.BILINEAR,align_corners=False)
 flayer21 = tf.cast(flayer21,dtype=tf.float64)
-
 dlayer3 = dl3.feedforward(tf.concat([flayer21,elayer2],3))
 flayer3 = fl3.feedforward(dlayer3)
 
 flayer31 = tf.image.resize_images(flayer3, [96, 96],method=tf.image.ResizeMethod.BICUBIC,align_corners=False)
 flayer31 = tf.cast(flayer31,dtype=tf.float64)
-
 dlayer4 = dl4.feedforward(tf.concat([flayer31,elayer1],3),stride=1)
 flayer5 = fl4.feedforward(dlayer4)
 
 cost0 = tf.reduce_mean(tf.square(flayer5-y))
-cost1 = tf.reduce_mean([sparse_cost0 ,sparse_cost1 ,sparse_cost2,sparse_cost3,sparse_cost4])
+cost1 = tf.reduce_mean([sparse_cost0 ,sparse_cost1 ,sparse_cost2,sparse_cost3,sparse_cost4,sparse_cost5])
 
 total_cost = cost0 + cost1
 auto_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(total_cost)
@@ -528,7 +518,6 @@ with tf.Session() as sess:
         for xx in range(len(sess_results)):
             f, axarr = plt.subplots(2, 3,figsize=(27,18))
 
-            # test_change_predict = (sess_results[xx]-sess_results[xx].min())/(sess_results[xx].max()-sess_results[xx].min())
             test_change_predict = sess_results[xx]
 
             plt.suptitle('Final Train Images : ' + str(xx) ,fontsize=20)
@@ -561,7 +550,6 @@ with tf.Session() as sess:
         for xx in range(len(sess_results)):
             f, axarr = plt.subplots(2, 3,figsize=(27,18))
         
-            # test_change_predict = (sess_results[xx]-sess_results[xx].min())/(sess_results[xx].max()-sess_results[xx].min())
             test_change_predict = sess_results[xx]
 
             plt.suptitle('Final Test Images : ' + str(xx) ,fontsize=20)
