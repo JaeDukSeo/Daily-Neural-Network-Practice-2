3ec7cba19fc912ba6311b7d560a4c742528e76c7
Jae <jae.duk.seo@ryerson.ca>
2018-08-04 08:37:36 +0900

    s

---
+++ a_starting_code/a_all_layers.py
@@ -0,0 +1,287 @@
+import tensorflow as tf
+import numpy as np
+import sys, os,cv2
+from sklearn.utils import shuffle
+from scipy.misc import imread,imresize
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
+from skimage.transform import resize
+from imgaug import augmenters as iaa
+import nibabel as nib
+import imgaug as ia
+from scipy.ndimage import zoom
+
+plt.style.use('seaborn-white')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+np.random.seed(6278)
+tf.set_random_seed(6728)
+ia.seed(6278)
+
+# ======= Activation Function  ==========
+def tf_elu(x): return tf.nn.elu(x)
+def d_tf_elu(x): return tf.cast(tf.greater(x,0),tf.float32)  + (tf_elu(tf.cast(tf.less_equal(x,0),tf.float32) * x) + 1.0)
+
+def tf_tanh(x): return tf.nn.tanh(x)
+def d_tf_tanh(x): return 1 - tf_tanh(x) ** 2
+
+def tf_sigmoid(x): return tf.nn.sigmoid(x) 
+def d_tf_sigmoid(x): return tf_sigmoid(x) * (1.0-tf_sigmoid(x))
+
+def tf_atan(x): return tf.atan(x)
+def d_tf_atan(x): return 1.0/(1.0 + x**2)
+
+def tf_iden(x): return x
+def d_tf_iden(x): return 1.0
+
+def tf_softmax(x): return tf.nn.softmax(x)
+def softabs(x): return tf.sqrt(x ** 2 + 1e-20)
+# ======= Activation Function  ==========
+
+# ====== miscellaneous =====
+# code from: https://github.com/tensorflow/tensorflow/issues/8246
+def tf_repeat(tensor, repeats):
+    """
+    Args:
+
+    input: A Tensor. 1-D or higher.
+    repeats: A list. Number of repeat for each dimension, length must be the same as the number of dimensions in input
+
+    Returns:
+    
+    A Tensor. Has the same type as input. Has the shape of tensor.shape * repeats
+    """
+    expanded_tensor = tf.expand_dims(tensor, -1)
+    multiples = [1] + repeats
+    tiled_tensor = tf.tile(expanded_tensor, multiples = multiples)
+    repeated_tesnor = tf.reshape(tiled_tensor, tf.shape(tensor) * repeats)
+    return repeated_tesnor
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+# ====== miscellaneous =====
+
+# ================= LAYER CLASSES =================
+class CNN():
+    
+    def __init__(self,k,inc,out,act=tf_elu,d_act=d_tf_elu):
+        self.w = tf.Variable(tf.random_normal([k,k,inc,out],stddev=0.05,seed=2,dtype=tf.float64))
+        self.m,self.v_prev = tf.Variable(tf.zeros_like(self.w)),tf.Variable(tf.zeros_like(self.w))
+        self.act,self.d_act = act,d_act
+
+    def getw(self): return [self.w]
+
+    def feedforward(self,input,stride=1,padding='SAME'):
+        self.input  = input
+        self.layer  = tf.nn.conv2d(input,self.w,strides=[1,stride,stride,1],padding=padding) 
+        self.layerA = self.act(self.layer)
+        return self.layerA 
+
+    def backprop(self,gradient,stride=1,padding='SAME'):
+        grad_part_1 = gradient 
+        grad_part_2 = self.d_act(self.layer) 
+        grad_part_3 = self.input
+
+        grad_middle = grad_part_1 * grad_part_2
+
+        grad = tf.nn.conv2d_backprop_filter(input = grad_part_3,filter_sizes = self.w.shape,out_backprop = grad_middle,
+            strides=[1,stride,stride,1],padding=padding
+        )
+
+        grad_pass = tf.nn.conv2d_backprop_input(input_sizes = [batch_size] + list(grad_part_3.shape[1:]),filter= self.w,out_backprop = grad_middle,
+            strides=[1,stride,stride,1],padding=padding
+        )
+
+        update_w = []
+        update_w.append(tf.assign( self.m,self.m*beta1 + (1-beta1) * (grad)   ))
+        update_w.append(tf.assign( self.v_prev,self.v_prev*beta2 + (1-beta2) * (grad ** 2)   ))
+        m_hat = self.m / (1-beta1)
+        v_hat = self.v_prev / (1-beta2)
+        adam_middel = learning_rate/(tf.sqrt(v_hat) + adam_e)
+        update_w.append(tf.assign(self.w,tf.subtract(self.w,tf.multiply(adam_middel,m_hat)  )))         
+
+        return grad_pass,update_w 
+
+class CNN_3D():
+    
+    def __init__(self,filter_depth,filter_height,filter_width,in_channels,out_channels,act=tf_elu,d_act=d_tf_elu):
+        self.w = tf.Variable(tf.random_normal([filter_depth,filter_height,filter_width,in_channels,out_channels],stddev=0.05,seed=2,dtype=tf.float64))
+        self.b = tf.Variable(tf.random_normal([out_channels],stddev=0.05,seed=2,dtype=tf.float64))
+        self.act,self.d_act = act,d_act
+    def getw(self): return [self.w]
+    def feedforward(self,input,stride=1,padding='SAME',res=True):
+        self.input  = input
+        self.layer  = tf.nn.conv3d(input,self.w,strides=[1,1,1,1,1],padding=padding) + self.b
+        self.layerA = self.act(self.layer)
+        if res:
+            return self.layerA + self.input
+        else:
+            return self.layerA 
+
+class CNN_Trans():
+    
+    def __init__(self,k,inc,out,act=tf_elu,d_act=d_tf_elu):
+        self.w = tf.Variable(tf.random_normal([k,k,inc,out],stddev=0.05,seed=2,dtype=tf.float64))
+        self.m,self.v_prev = tf.Variable(tf.zeros_like(self.w)),tf.Variable(tf.zeros_like(self.w))
+        self.act,self.d_act = act,d_act
+
+    def getw(self): return [self.w]
+
+    def feedforward(self,input,stride=1,padding='SAME'):
+        self.input  = input
+        output_shape2 = self.input.shape[2].value * stride
+        self.layer  = tf.nn.conv2d_transpose(
+            input,self.w,output_shape=[batch_size,output_shape2,output_shape2,self.w.shape[2].value],
+            strides=[1,stride,stride,1],padding=padding) 
+        self.layerA = self.act(self.layer)
+        return self.layerA 
+
+    def backprop(self,gradient,stride=1,padding='SAME'):
+        grad_part_1 = gradient 
+        grad_part_2 = self.d_act(self.layer) 
+        grad_part_3 = self.input
+
+        grad_middle = grad_part_1 * grad_part_2
+
+        grad = tf.nn.conv2d_backprop_filter(input = grad_middle,
+            filter_sizes = self.w.shape,out_backprop = grad_part_3,
+            strides=[1,stride,stride,1],padding=padding
+        )
+
+        grad_pass = tf.nn.conv2d(
+            input=grad_middle,filter = self.w,strides=[1,stride,stride,1],padding=padding
+        )
+        
+        update_w = []
+        update_w.append(tf.assign( self.m,self.m*beta1 + (1-beta1) * (grad)   ))
+        update_w.append(tf.assign( self.v_prev,self.v_prev*beta2 + (1-beta2) * (grad ** 2)   ))
+        m_hat = self.m / (1-beta1)
+        v_hat = self.v_prev / (1-beta2)
+        adam_middel = learning_rate/(tf.sqrt(v_hat) + adam_e)
+        update_w.append(tf.assign(self.w,tf.subtract(self.w,tf.multiply(adam_middel,m_hat)  )))         
+
+        return grad_pass,update_w 
+
+class LSTM_CNN():
+    
+    def __init__(self):
+        pass
+
+class FNN():
+    
+    def __init__(self,input_dim,hidden_dim,act,d_act):
+        self.w = tf.Variable(tf.random_normal([input_dim,hidden_dim], stddev=0.05,seed=2,dtype=tf.float64))
+        self.m,self.v_prev = tf.Variable(tf.zeros_like(self.w)),tf.Variable(tf.zeros_like(self.w))
+        self.v_hat_prev = tf.Variable(tf.zeros_like(self.w))
+        self.act,self.d_act = act,d_act
+
+    def getw(self): return [self.w]
+
+    def feedforward(self,input=None):
+        self.input = input
+        self.layer = tf.matmul(input,self.w)
+        self.layerA = self.act(self.layer)
+        return self.layerA
+
+    def backprop(self,gradient=None):
+        grad_part_1 = gradient 
+        grad_part_2 = self.d_act(self.layer) 
+        grad_part_3 = self.input
+
+        grad_middle = grad_part_1 * grad_part_2
+        grad = tf.matmul(tf.transpose(grad_part_3),grad_middle)
+        grad_pass = tf.matmul(tf.multiply(grad_part_1,grad_part_2),tf.transpose(self.w))
+
+        update_w = []
+        update_w.append(tf.assign( self.m,self.m*beta1 + (1-beta1) * (grad)   ))
+        update_w.append(tf.assign( self.v_prev,self.v_prev*beta2 + (1-beta2) * (grad ** 2)   ))
+        m_hat = self.m / (1-beta1)
+        v_hat = self.v_prev / (1-beta2)
+        adam_middel = learning_rate/(tf.sqrt(v_hat) + adam_e)
+        update_w.append(tf.assign(self.w,tf.subtract(self.w,tf.multiply(adam_middel,m_hat)  )))     
+
+        return grad_pass,update_w  
+
+class ICA_Layer():
+
+    def __init__(self,inc):
+        self.w_ica = tf.Variable(tf.random_normal([inc,inc],stddev=0.05,seed=2)) 
+        # self.w_ica = tf.Variable(tf.eye(inc)*0.0001) 
+
+    def feedforward(self,input):
+        self.input = input
+        self.ica_est = tf.matmul(input,self.w_ica)
+        self.ica_est_act = tf_atan(self.ica_est)
+        return self.ica_est_act
+
+    def backprop(self):
+        grad_part_2 = d_tf_atan(self.ica_est)
+        grad_part_3 = self.input
+
+        grad_pass = tf.matmul(grad_part_2,tf.transpose(self.w_ica))
+        g_tf = tf.linalg.inv(tf.transpose(self.w_ica)) - (2/batch_size) * tf.matmul(tf.transpose(self.input),self.ica_est_act)
+
+        update_w = []
+        update_w.append(tf.assign(self.w_ica,self.w_ica+0.2*g_tf))
+
+        return grad_pass,update_w  
+
+class Sparse_Filter_Layer():
+    
+    def __init__(self,outc,changec):
+        self.w = tf.Variable(tf.random_normal([outc,changec],stddev=1.0,seed=2,dtype=tf.float64))
+        self.epsilon = 1e-20
+
+    def getw(self): return self.w
+
+    def soft_abs(self,value):
+        return tf.sqrt(value ** 2 + self.epsilon)
+
+    def feedforward(self,input):
+        self.sparse_layer  = tf.matmul(input,self.w)
+        second = self.soft_abs(self.sparse_layer )
+        third  = tf.divide(second,tf.sqrt(tf.reduce_sum(second**2,axis=0)+self.epsilon))
+        four = tf.divide(third,tf.sqrt(tf.reduce_sum(third**2,axis=1)[:,tf.newaxis] +self.epsilon))
+        self.cost_update = tf.reduce_mean(four)
+        return self.sparse_layer ,self.cost_update
+# ================= LAYER CLASSES =================
+
+# read the data
+all_brain_data= np.load('all_brain_data.npy')
+all_mask_data = np.load('all_mask_data.npy')
+
+all_brain_data = (all_brain_data-all_brain_data.min(axis=(1,2,3))[:, np.newaxis, np.newaxis, np.newaxis] ) / \
+                (all_brain_data.max(axis=(1,2,3))[:, np.newaxis, np.newaxis, np.newaxis] - all_brain_data.min(axis=(1,2,3))[:, np.newaxis, np.newaxis, np.newaxis] +1e-10 ) 
+
+all_mask_data = (all_mask_data-all_mask_data.min(axis=(1,2,3))[:, np.newaxis, np.newaxis, np.newaxis] ) / \
+                (all_mask_data.max(axis=(1,2,3))[:, np.newaxis, np.newaxis, np.newaxis] - all_mask_data.min(axis=(1,2,3))[:, np.newaxis, np.newaxis, np.newaxis] +1e-10 ) 
+
+split_number = 18
+train_batch = all_brain_data[:split_number]
+train_label = all_mask_data[:split_number]
+test_batch = all_brain_data[split_number:]
+test_label =all_mask_data[split_number:]
+
+# print out the data shape
+print('-----------------------')
+print(train_batch.shape)
+print(train_batch.max(axis=(1,2,3)).max())
+print(train_batch.min(axis=(1,2,3)).max())
+print(train_label.shape)
+print(train_label.max(axis=(1,2,3)).max())
+print(train_label.min(axis=(1,2,3)).max())
+
+print(test_batch.shape)
+print(test_batch.max(axis=(1,2,3)).max())
+print(test_batch.min(axis=(1,2,3)).max())
+print(test_label.shape)
+print(test_label.max(axis=(1,2,3)).max())
+print(test_label.min(axis=(1,2,3)).max())
+print('-----------------------')
+
+
+
+
+
+# -- end code --
