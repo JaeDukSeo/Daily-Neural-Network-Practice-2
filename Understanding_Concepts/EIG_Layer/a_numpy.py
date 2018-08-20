264da00972e396c1f0a9dd1ff557f6f1a5915700
Jae <jae.duk.seo@ryerson.ca>
2018-08-20 12:16:33 +0900

    s

--- Understanding_Concepts/EIG_Layer/a_numpy.py
+++ Understanding_Concepts/EIG_Layer/a_numpy.py
@@ -15,63 +15,6 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 from tensorflow.examples.tutorials.mnist import input_data
 np.random.seed(789)
 
-# ====== miscellaneous =====
-# code from: https://github.com/tensorflow/tensorflow/issues/8246
-def tf_repeat(tensor, repeats):
-    """
-    Args:
-
-    input: A Tensor. 1-D or higher.
-    repeats: A list. Number of repeat for each dimension, length must be the same as the number of dimensions in input
-
-    Returns:
-
-    A Tensor. Has the same type as input. Has the shape of tensor.shape * repeats
-    """
-    expanded_tensor = tf.expand_dims(tensor, -1)
-    multiples = [1] + repeats
-    tiled_tensor = tf.tile(expanded_tensor, multiples = multiples)
-    repeated_tesnor = tf.reshape(tiled_tensor, tf.shape(tensor) * repeats)
-    return repeated_tesnor
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-# ====== miscellaneous =====
-
-# # data
-# mnist = input_data.read_data_sets('../../Dataset/MNIST/', one_hot=True)
-# x_data, train_label, y_data, test_label = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
-# x_data_added,x_data_added_label = mnist.validation.images,mnist.validation.labels
-# x_data = x_data.reshape(-1, 28, 28, 1)  # 28x28x1 input img
-# y_data = y_data.reshape(-1, 28, 28, 1)  # 28x28x1 input img
-# x_data_added = x_data_added.reshape(-1, 28, 28, 1)
-#
-# x_data = np.vstack((x_data,x_data_added))
-# train_label = np.vstack((train_label,x_data_added_label))
-# train_batch = np.zeros((60000,28,28,1))
-# test_batch = np.zeros((10000,28,28,1))
-#
-# for x in range(len(x_data)):
-#     train_batch[x,:,:,:] = np.expand_dims(imresize(x_data[x,:,:,0],(28,28)),axis=3)
-# for x in range(len(y_data)):
-#     test_batch[x,:,:,:] = np.expand_dims(imresize(y_data[x,:,:,0],(28,28)),axis=3)
-#
-# # print out the data shape and the max and min value
-# print(train_batch.shape)
-# print(train_batch.max())
-# print(train_batch.min())
-# print(train_label.shape)
-# print(train_label.max())
-# print(train_label.min())
-# print(test_batch.shape)
-# print(test_batch.max())
-# print(test_batch.min())
-# print(test_label.shape)
-# print(test_label.max())
-# print(test_label.min())
-
 
 def np_sig(x): return 1.0/(1.0+np.exp(-x))
 def d_np_sig(x): return np_sig(x) * (1.0 - np_sig(x))
@@ -103,27 +46,67 @@ class FNN_numpy():
         adam_mid = learning_rate / (np.sqrt(v_hat) + adam_e) * m_hat
         self.w = self.w - adam_mid
         return grad_pass
-
 class whitening_layer():
 
     def __init__(self):
         self.moving_sigma = 0
         self.moving_mean = 0
 
-    def feedforward(self,input):
-        self.u = np.expand_dims(np.mean(input,axis=1),1)
-        self.sigma = (input - self.u).dot((input - self.u).T) / batch_size
+    def feedforward(self,input,is_training=True):
+        self.input = input
+        self.mean = np.mean(input,axis=0)
+        self.sigma = (input - self.mean).T.dot(input - self.mean) / input.shape[1]
         self.eigenval,self.eigvector = np.linalg.eigh(self.sigma)
-        self.U = np.diag(1. / np.sqrt(self.eigenval + white_e)).dot(self.eigvector.T)
-        self.whiten = self.U.dot(input-self.u)
-        self.zca = self.eigvector.dot(self.whiten)
+        self.U = self.eigvector.dot(np.diag(1. / np.sqrt(self.eigenval + white_e)))
+        self.whiten = (input - self.mean).dot(self.U)
+        self.zca = self.whiten.dot(self.eigvector)
         return self.zca
 
     def backprop(self,grad):
-        pass
+        dwhiten = grad.dot(self.eigvector.T)
+        dU = (self.input-self.mean).T.dot(dwhiten)
+        deigenval = self.eigvector.T.dot(dU).dot(np.diag(-0.5 * 1/(self.eigenval ** 1.5)))
+        deigvector = self.whiten.T.dot(grad) + dU.dot(np.diag(1. / np.sqrt(self.eigenval + white_e) ).T)
 
+        shape_eig = self.eigenval.shape[0]
+        Iden = np.eye(shape_eig)
+        E = np.ones((shape_eig,1)).dot(np.expand_dims(self.eigenval.T,0))-np.expand_dims(self.eigenval,1).dot(np.ones((1,shape_eig)))
+        F = 1.0/(E+Iden) - Iden
+        deigenval_zero = np.eye(deigenval.shape[0]) * deigenval
+        dsigma = self.eigvector.dot( F.T * (self.eigvector.dot(deigvector))+deigenval_zero).dot(self.eigvector.T)
+
+        symm_sigma = 0.5 * (dsigma.T + dsigma)
+        dmean = -np.sum(dwhiten,0).dot(self.U.T) + (-2/shape_eig) * np.mean(self.input - self.mean,0).dot(symm_sigma)
+
+        dInput = dwhiten.dot(self.U.T) + (2/shape_eig) * (self.input - self.mean).dot(symm_sigma) + 1/shape_eig * dmean
+        return dInput
+
+# # data
+mnist = input_data.read_data_sets('../../Dataset/MNIST/', one_hot=True)
+x_data, train_label, y_data, test_label = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+x_data_added,x_data_added_label = mnist.validation.images,mnist.validation.labels
+
+x_data = np.vstack((x_data,x_data_added))
+train_batch = x_data
+train_label = np.vstack((train_label,x_data_added_label))
+test_batch = y_data
+
+# print out the data shape and the max and min value
+print(train_batch.shape)
+print(train_batch.max())
+print(train_batch.min())
+print(train_label.shape)
+print(train_label.max())
+print(train_label.min())
+print(test_batch.shape)
+print(test_batch.max())
+print(test_batch.min())
+print(test_label.shape)
+print(test_label.max())
+print(test_label.min())
 
 # hyper
+num_epoch = 100
 batch_size = 100
 
 learning_rate = 0.0001
@@ -133,13 +116,20 @@ white_e = 1e-10
 # class
 l0 = FNN_numpy(784,256)
 l1 = whitening_layer()
+l2 = FNN_numpy(256,10)
 
-# graph
-temp = np.ones((50,784))
+# train
+for iter in range(num_epoch):
 
-layer0 = l0.feedforward(temp)
-layer1 = l1.feedforward(layer0)
+    for current_batch_index in range(0,len(train_batch),batch_size):
 
+        current_data = train_batch[current_batch_index:current_batch_index+batch_size]
+        current_data_label = train_label[current_batch_index:current_batch_index+batch_size]
+
+        layer0 = l0.feedforward(current_data)
+        layer1 = l1.feedforward(layer0)
+        layer2 = l2.feedforward(layer1)
+        sys.exit()
 
 
 
