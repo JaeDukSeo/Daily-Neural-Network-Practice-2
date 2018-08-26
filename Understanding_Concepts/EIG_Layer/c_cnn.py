2a5365a5167389a0a3762baba458288c1bfae40b
Jae <jae.duk.seo@ryerson.ca>
2018-08-26 21:27:15 +0900

    s

--- Understanding_Concepts/EIG_Layer/c_cnn.py
+++ Understanding_Concepts/EIG_Layer/c_cnn.py
@@ -10,6 +10,7 @@ from imgaug import augmenters as iaa
 import imgaug as ia
 from scipy.ndimage import zoom
 import seaborn as sns
+from scipy.signal import convolve
 
 np.random.seed(0)
 ia.random.seed(0)
@@ -17,6 +18,55 @@ np.set_printoptions(precision = 3,suppress =True)
 old_v = tf.logging.get_verbosity()
 tf.logging.set_verbosity(tf.logging.ERROR)
 from tensorflow.examples.tutorials.mnist import input_data
+
+def get_im2col_indices(x_shape, field_height, field_width, padding=1, stride=1):
+    # First figure out what the size of the output should be
+    N, C, H, W = x_shape
+    assert (H + 2 * padding - field_height) % stride == 0
+    assert (W + 2 * padding - field_height) % stride == 0
+    out_height = int((H + 2 * padding - field_height) / stride + 1)
+    out_width = int((W + 2 * padding - field_width) / stride + 1)
+
+    i0 = np.repeat(np.arange(field_height), field_width)
+    i0 = np.tile(i0, C)
+    i1 = stride * np.repeat(np.arange(out_height), out_width)
+    j0 = np.tile(np.arange(field_width), field_height * C)
+    j1 = stride * np.tile(np.arange(out_width), out_height)
+    i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+    j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+
+    k = np.repeat(np.arange(C), field_height * field_width).reshape(-1, 1)
+
+    return (k.astype(int), i.astype(int), j.astype(int))
+
+def im2col_indices(x, field_height, field_width, padding=1, stride=1):
+    """ An implementation of im2col based on some fancy indexing """
+    # Zero-pad the input
+    p = padding
+    x_padded = np.pad(x, ((0, 0), (0, 0), (p, p), (p, p)), mode='constant')
+
+    k, i, j = get_im2col_indices(x.shape, field_height, field_width, padding, stride)
+
+    cols = x_padded[:, k, i, j]
+    C = x.shape[1]
+    cols = cols.transpose(1, 2, 0).reshape(field_height * field_width * C, -1)
+    return cols
+
+def col2im_indices(cols, x_shape, field_height=3, field_width=3, padding=1,
+                   stride=1):
+    """ An implementation of col2im based on fancy indexing and np.add.at """
+    N, C, H, W = x_shape
+    H_padded, W_padded = H + 2 * padding, W + 2 * padding
+    x_padded = np.zeros((N, C, H_padded, W_padded), dtype=cols.dtype)
+    k, i, j = get_im2col_indices(x_shape, field_height, field_width, padding, stride)
+    cols_reshaped = cols.reshape(C * field_height * field_width, -1, N)
+    cols_reshaped = cols_reshaped.transpose(2, 0, 1)
+    np.add.at(x_padded, (slice(None), k, i, j), cols_reshaped)
+    if padding == 0:
+        return x_padded
+    return x_padded[:, :, padding:-padding, padding:-padding]
+
+
 
 # def: relu activations
 def np_relu(x): return x * (x > 0)
@@ -73,6 +123,16 @@ class np_FNN():
         self.b = self.b - adam_middleb
 
         return grad_pass
+
+class np_CNN():
+
+    def __init__(self,inc,outc,batch_size,act=np_relu,d_act = d_np_relu):
+        self.w = r.normal()
+        self.b = r.normal()
+        self.m,self.v = np.zeros_like(self.w),np.zeros_like(self.w)
+        self.mb,self.vb = np.zeros_like(self.b),np.zeros_like(self.b)
+        self.act = act; self.d_act = d_act
+        self.batch_size = batch_size
 
 # def: centering layer
 class centering_layer():
@@ -135,6 +195,7 @@ def stable_softmax(x,axis=None):
     e_x = np.exp(x - np.max(x,axis=1)[:,np.newaxis])
     return e_x / e_x.sum(axis=1)[:,np.newaxis]
 
+
 # mnist = input_data.read_data_sets('../../Dataset/MNIST/', one_hot=True)
 mnist = input_data.read_data_sets('../../Dataset/fashionmnist/',one_hot=True)
 train_data, train_label, test_data, test_label = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
@@ -152,11 +213,66 @@ print(test_label.shape)
 print(test_label.min(),test_label.max())
 print('-----------------------')
 
+def conv_forward(X, W, b, stride=1, padding=1):
+    # cache = W, b, stride, padding
+    n_filters, d_filter, h_filter, w_filter = W.shape
+    n_x, d_x, h_x, w_x = X.shape
+    h_out = (h_x - h_filter + 2 * padding) / stride + 1
+    w_out = (w_x - w_filter + 2 * padding) / stride + 1
+
+    if not h_out.is_integer() or not w_out.is_integer():
+        raise Exception('Invalid output dimension!')
+
+    h_out, w_out = int(h_out), int(w_out)
+
+    X_col = im2col_indices(X, h_filter, w_filter, padding=padding, stride=stride)
+    W_col = W.reshape(n_filters, -1)
+
+    out = W_col @ X_col + b
+    out = out.reshape(n_filters, h_out, w_out, n_x)
+    out = out.transpose(3, 0, 1, 2)
+
+    cache = (X, W, b, stride, padding, X_col)
+
+    return out, cache
+
+
+def conv_backward(dout, cache):
+    X, W, b, stride, padding, X_col = cache
+    n_filter, d_filter, h_filter, w_filter = W.shape
+
+    db = np.sum(dout, axis=(0, 2, 3))
+    db = db.reshape(n_filter, -1)
+
+    dout_reshaped = dout.transpose(1, 2, 3, 0).reshape(n_filter, -1)
+    dW = dout_reshaped @ X_col.T
+    dW = dW.reshape(W.shape)
+
+    W_reshape = W.reshape(n_filter, -1)
+    dX_col = W_reshape.T @ dout_reshaped
+    dX = col2im_indices(dX_col, X.shape, h_filter, w_filter, padding=padding, stride=stride)
+
+    return dX, dW, db
+
+
+train_data = train_data.reshape(60000,28,28)
+temp = train_data[0][np.newaxis,np.newaxis,:,:]
+w = np.random.randn((3,1,3,3))
+b = np.random.randn((3,1))
+print(temp.shape)
+print(w.shape)
+print(b.shape)
+
+ffff = conv_forward(temp,w,b)
+print(ffff.shape)
+
+sys.exit()
+
 # hyper
 num_epoch = 30
-batch_size = 1250
+batch_size = 2000
 learning_rate = 0.0003
-lamda = 0.001
+lamda = 0.005
 print_size  = 1
 beta1,beta2,adam_e = 0.9,0.999,1e-20
 
