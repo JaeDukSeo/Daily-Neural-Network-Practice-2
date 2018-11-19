7081a9b663f328c58c2ed9ed4f5ceb9061507219
Jae <jae.duk.seo@ryerson.ca>
2018-11-19 12:05:34 -0500

    winter cleaning

--- Medical_EXP/hospital/a.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-
-
-
-
-# -- end code --
---
+++ Medical_exploration/hospital/a.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+
+
+
+# -- end code --
