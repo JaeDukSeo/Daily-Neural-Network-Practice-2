8f8f9cda26fd03138028dd6892719529c997b7eb
Jae <jae.duk.seo@ryerson.ca>
2018-11-19 17:50:22 -0500

    s

--- NeuralNetwork/principal_component_analysis_regularization/comparing.py
+++ NeuralNetwork/principal_component_analysis_regularization/comparing.py
@@ -31,16 +31,41 @@ t1 = f * (utgu - utgu.T) * s[..., np.newaxis, :] + i * gs[..., :, np.newaxis] + 
 t1 = U @ t1 @ V
 
 i_minus_uut = np.eye(5) - U @ U.T
-t1 = t1 + i_minus_uut @ gu @ (V *    (1/s)[..., :, np.newaxis])
+t1 = t1 + i_minus_uut @ gu @ (V /s [..., :, np.newaxis])
+t11 = t1.copy()
+i_minus_vvt = np.eye(3) - V @ V.T
 
+print(i_minus_vvt)
+print((U / s[..., np.newaxis, :]) @ gv.T @i_minus_vvt)
+t1 = t1 + (U / s[..., np.newaxis, :]) @ gv.T @i_minus_vvt
 
 print('-------------------------')
 print(
     t1 
 )
+print('-------------------------')
+print(
+    t11 
+)
+print('-------------------------')
 print(
     np.allclose(t1,grad_ssvd(data))
 )
 
+import tensorflow as tf
+
+def tf_svd(data):
+    S,U,VT   = tf.linalg.svd(data,full_matrices=False)
+    S = tf.diag(S)
+    data_hat = U @ S @ tf.transpose(VT)
+    return data_hat
+
+
+sess = tf.InteractiveSession()
+one  = tf_svd(data)
+one  = one.eval() 
+print(
+    np.allclose(one,data)
+)
 
 # -- end code --
