7e472b4cdc99a1cbe29e8e9ed8b541eb36ef7d91
Jae <jae.duk.seo@ryerson.ca>
2019-01-05 12:51:25 -0500

    s

--- Class Stuff/Sparsity in Gradient Point of View/blog/A/makegif.py
+++ Class Stuff/Sparsity in Gradient Point of View/blog/A/makegif.py
@@ -9,7 +9,7 @@ print(file_list[10].split('.')[0].split('\\')[1])
 import sys
 sys.exit()
 
-list.sort(file_list, key=lambda x: int(x.split('.')[0].split('\\'[2]) )) # Sort the images by #, this may need to be tweaked for your use case
+list.sort(file_list, key=lambda x: int(x.split('.')[0].split('\\')[1] )) # Sort the images by #, this may need to be tweaked for your use case
 clip = mpy.ImageSequenceClip(file_list, fps=fps)
 clip.write_gif('gradientp/{}.gif'.format(gif_name), fps=fps)
 
