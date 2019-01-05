e2d5cede3522a888517004b27188d05181af498e
Jae <jae.duk.seo@ryerson.ca>
2019-01-05 12:47:35 -0500

    s

--- Class Stuff/Sparsity in Gradient Point of View/blog/A/makegif.py
+++ Class Stuff/Sparsity in Gradient Point of View/blog/A/makegif.py
@@ -11,9 +11,9 @@ clip.write_gif('gradientp/{}.gif'.format(gif_name), fps=fps)
 file_list = glob.glob('gradientw/*.png') # Get all the pngs in the current directory
 list.sort(file_list, key=lambda x: int(x.split('.')[0] )) # Sort the images by #, this may need to be tweaked for your use case
 clip = mpy.ImageSequenceClip(file_list, fps=fps)
-clip.write_gif('gradientp/{}.gif'.format(gif_name), fps=fps)
+clip.write_gif('gradientw/{}.gif'.format(gif_name), fps=fps)
 
-file_list = glob.glob('gradientp/*.png') # Get all the pngs in the current directory
+file_list = glob.glob('gradilayerentp/*.png') # Get all the pngs in the current directory
 list.sort(file_list, key=lambda x: int(x.split('.')[0] )) # Sort the images by #, this may need to be tweaked for your use case
 clip = mpy.ImageSequenceClip(file_list, fps=fps)
 clip.write_gif('gradientp/{}.gif'.format(gif_name), fps=fps)
