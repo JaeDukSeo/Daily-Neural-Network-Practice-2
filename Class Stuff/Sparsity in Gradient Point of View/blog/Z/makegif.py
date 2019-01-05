b4c1bdc83d7b180a27dda0fa6bc3ab9917a90e07
Jae <jae.duk.seo@ryerson.ca>
2019-01-05 18:23:05 -0500

    s

--- Class Stuff/Sparsity in Gradient Point of View/blog/Z/makegif.py
+++ Class Stuff/Sparsity in Gradient Point of View/blog/Z/makegif.py
@@ -2,7 +2,7 @@ import glob
 import moviepy.editor as mpy
 
 # 5
-gif_name = 'gif'; fps = 25
+gif_name = 'gif'; fps = 5
 
 file_list = glob.glob('gradientp/*.png') # Get all the pngs in the current directory
 list.sort(file_list, key=lambda x: int(x.split('.')[0].split('\\')[1] )) # Sort the images by #, this may need to be tweaked for your use case
